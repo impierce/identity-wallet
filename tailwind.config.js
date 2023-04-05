db4dc78a8cc60e8c80b78c9474129caeb4092d5d
Daniel Mader <45594838+daniel-mader@users.noreply.github.com>
2023-04-06 00:33:58 +0200

    chore: change default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'sans': ['Satoshi', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: []
 };
