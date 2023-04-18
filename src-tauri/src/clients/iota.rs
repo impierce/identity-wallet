8de6c0042430e3a1ebff9d00498c16c0244d3c3d
Daniel Mader <45594838+daniel-mader@users.noreply.github.com>
2023-04-18 18:01:27 +0200

    chore: add metadata to package, remove unused code

--- src-tauri/src/clients/iota.rs
+++
@@ -1,8 +0,0 @@
-use iota_client::Client;
-
-pub async fn create_iota_identity() -> anyhow::Result<()> {
-    static API_ENDPOINT: &str = "http://127.0.0.1:14265";
-
-    let client: Client = Client::builder().with_primary_node(API_ENDPOINT, None)?.finish()?;
-    Ok(())
-}
