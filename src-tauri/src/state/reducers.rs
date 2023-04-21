75b386061b0a8730c1f48c9a213027a7a2f67be4
Daniel Mader <daniel.mader@impierce.com>
2023-04-21 16:20:21 +0200

    fix: reducers: return error instead of panicking

--- src-tauri/src/state/reducers.rs
+++ src-tauri/src/state/reducers.rs
@@ -1,25 +1,24 @@
 use crate::state::actions::Action;
 use crate::state::state::{AppState, Profile, StateStatus};
-use tracing::{warn, info};
+use tracing::info;
 
 /// Sets the locale to the given value. If the locale is not supported yet, the current locale will stay unchanged.
 pub fn set_locale(state: &AppState, action: Action) -> anyhow::Result<()> {
-    let payload = action.payload.unwrap();
-    let locale = payload["locale"].as_str().expect("unable to read locale from json payload");
-    let supported_locales = vec!["en", "nl", "de"];
-    if !supported_locales.contains(&locale) {
-        warn!("unsupported locale: `{}`", locale);
-    } else {
-        *state.locale.lock().unwrap() = locale.to_string();
-        info!("locale set to: `{}`", locale);
-    }
+    let payload = action.payload.ok_or(anyhow::anyhow!("unable to read payload"))?;
+    let locale = payload["locale"]
+        .as_str()
+        .ok_or(anyhow::anyhow!("unable to read locale from json payload"))?;
+    *state.locale.lock().unwrap() = locale.to_string();
+    info!("locale set to: `{}`", locale);
     Ok(())
 }
 
 /// Creates a new profile with a new DID (using the did:key method) and sets it as the active profile.
 pub fn create_did_key(state: &AppState, action: Action) -> anyhow::Result<()> {
     let payload = action.payload.unwrap();
-    let display_name = payload["display_name"].as_str().expect("unable to read display_name from json payload");
+    let display_name = payload["display_name"]
+        .as_str()
+        .ok_or(anyhow::anyhow!("unable to read display_name from json payload"))?;
     let mock_profile = Profile {
         display_name: display_name.to_string(),
         primary_did: "did:mock:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK".to_string(),
@@ -48,32 +47,55 @@ mod tests {
     fn test_set_locale() {
         let state = AppState::default();
 
-        set_locale(
+        assert!(set_locale(
             &state,
             Action {
                 r#type: ActionType::SetLocale,
                 payload: Some(json!({"locale": "nl"})),
             },
         )
-        .unwrap();
-
+        .is_ok());
         assert_eq!(*state.status.lock().unwrap(), StateStatus::Stable);
         assert_eq!(*state.locale.lock().unwrap(), "nl".to_string());
+    }
+
+    #[test]
+    fn test_set_locale_with_invalid_payload() {
+        let state = AppState::default();
+
+        // Assert that a `SetLocale` action without a payload returns an error.
+        assert!(set_locale(
+            &state,
+            Action {
+                r#type: ActionType::SetLocale,
+                payload: None,
+            },
+        )
+        .is_err());
+
+        // Assert that a `SetLocale` action with an invalid payload returns an error.
+        assert!(set_locale(
+            &state,
+            Action {
+                r#type: ActionType::SetLocale,
+                payload: Some(json!({"foo": "bar"})),
+            },
+        )
+        .is_err());
     }
 
     #[test]
     fn test_create_did_key() {
         let state = AppState::default();
 
-        create_did_key(
+        assert!(create_did_key(
             &state,
             Action {
                 r#type: ActionType::CreateNew,
                 payload: Some(json!({"display_name": "Ferris"})),
             },
         )
-        .unwrap();
-
+        .is_ok());
         assert_eq!(*state.status.lock().unwrap(), StateStatus::Stable);
         assert_eq!(
             *state.active_profile.lock().unwrap(),
@@ -91,19 +113,19 @@ mod tests {
             active_profile: Some(Profile {
                 display_name: "Ferris".to_string(),
                 primary_did: "did:mock:z6MkhaXgBZDvotDkL5257faiztiGiC2QtKLGpbnnEGta2doK".to_string(),
-            }).into(),
+            })
+            .into(),
             locale: "nl".to_string().into(),
         };
 
-        reset_state(
+        assert!(reset_state(
             &state,
             Action {
                 r#type: ActionType::Reset,
                 payload: None,
             },
         )
-        .unwrap();
-
+        .is_ok());
         assert_eq!(*state.status.lock().unwrap(), StateStatus::Stable);
         assert_eq!(*state.active_profile.lock().unwrap(), None);
         assert_eq!(*state.locale.lock().unwrap(), "en".to_string());
