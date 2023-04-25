18d0e4273602f1c7a457bd5303495749839023f3
Daniel Mader <daniel.mader@impierce.com>
2023-04-25 15:47:53 +0200

    refactor: initialize persistence on app start

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -24,6 +24,10 @@ pub fn run() {
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![handle_action])
+        .setup(|app| {
+            initialize_storage(app.handle()).ok();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
