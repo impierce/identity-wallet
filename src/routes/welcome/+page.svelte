03e720b6365ac15737898a6dc436e9cc011945af
Daniel Mader <daniel.mader@impierce.com>
2023-06-20 17:23:35 +0200

    fix: apply frontend fixes from review

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -32,36 +32,15 @@
   <div class="grid w-full max-w-sm items-center gap-1.5">
     <Label for="name">Name</Label>
     <Input type="text" id="name" placeholder="" bind:value={display_name} />
-    <!-- <p class="text-sm text-muted-foreground">You can change this later.</p> -->
+    <p class="text-muted-foreground text-sm">You can change this later.</p>
   </div>
-  <!-- <p data-testid="label-prompt-username" class="text-slate-600">{$LL.PROMPT_NAME()}</p>
-  <div>
-    <input
-      type="text"
-      data-testid="input-username"
-      class="w-full rounded-lg border px-4 py-2 shadow focus:outline-none focus:ring-2 focus:ring-violet-600"
-      placeholder=""
-      bind:this={usernameInput}
-    />
-  </div> -->
 
   <div class="grid w-full max-w-sm items-center gap-1.5">
     <Label for="password">Password</Label>
     <Input type="password" id="password" placeholder="" bind:value={password} />
-    <p class="text-muted-foreground text-sm">Choose a strong password.</p>
+    <p class="text-muted-foreground text-sm">Please choose a strong password.</p>
   </div>
-  <!-- <p data-testid="label-prompt-password" class="text-slate-600">Choose a new password</p>
-  <div>
-    <input
-      type="password"
-      data-testid="input-password"
-      class="w-full rounded-lg border px-4 py-2 shadow focus:outline-none focus:ring-2 focus:ring-violet-600"
-      placeholder=""
-      bind:this={passwordInput}
-    />
-  </div> -->
 
-  <!-- <Button label={$LL.CREATE_IDENTITY()} on:clicked={createProfile} /> -->
   <Button disabled={loading} on:click={createProfile}>
     {#if loading}
       <div class="mr-2">
