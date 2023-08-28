63a124ef320ddd14a0849ee327f655cf0ec18c05
nanderstabel <nander.stabel@impierce.com>
2023-08-28 23:31:08 +0200

    fix: clear required_spec_claims

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -94,5 +94,7 @@ pub fn get_unverified_jwt_claims(jwt: &serde_json::Value) -> serde_json::Value {
     let key = DecodingKey::from_secret(&[]);
     let mut validation = Validation::new(Algorithm::EdDSA);
     validation.insecure_disable_signature_validation();
+    validation.validate_exp = false;
+    validation.required_spec_claims.clear();
     decode(jwt.as_str().unwrap(), &key, &validation).unwrap().claims
 }
