3c344e69c9a3fa4395b474d922d3493830acd55c
Daniel Mader <daniel.mader@impierce.com>
2023-09-06 08:26:25 +0200

    ci: release version 0.2.5

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.impierce.identity_wallet"
         minSdk = 24
         targetSdk = 33
-        versionCode = 15
-        versionName = "0.2.4"
+        versionCode = 16
+        versionName = "0.2.5"
     }
     signingConfigs {
         create("release") {
