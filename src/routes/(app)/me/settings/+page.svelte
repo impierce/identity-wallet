3ade1230f0dc28a5a1094053f5b5e76619a72e81
Daniel Mader <daniel.mader@impierce.com>
2023-09-14 17:54:58 +0200

    fix: WIP

--- src/routes/(app)/me/settings/+page.svelte
+++ src/routes/(app)/me/settings/+page.svelte
@@ -21,7 +21,7 @@
 </script>
 
 <TopNavigation on:back={() => history.back()} title="Settings" />
-<div class="content-height flex flex-col bg-silver dark:bg-navy">
+<div class="content-height flex flex-col justify-between bg-silver dark:bg-navy">
   <div class="flex flex-col space-y-[10px] px-4 py-5" in:fly={{ x: 32, opacity: 1 }}>
     <!-- Account -->
     <p class="text-[14px]/[22px] font-medium text-slate-500">{$LL.ACCOUNT()}</p>
