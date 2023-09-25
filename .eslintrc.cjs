8f2f9655f3d30318f138a68f26f8476ab1c67248
Daniel Mader <daniel.mader@impierce.com>
2023-09-25 14:34:18 +0200

    chore: update eslint plugins and config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,28 @@
 module.exports = {
-  root: true,
   parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs'],
-  overrides: [
-    {
-      files: ['*.svelte'],
-      processor: 'svelte3/svelte3'
-    }
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+    'plugin:svelte/recommended',
+    'prettier'
   ],
-  settings: {
-    'svelte3/typescript': () => require('typescript')
-  },
+  root: true,
+  // ignorePatterns: ['*.cjs'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020
   },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ],
   env: {
     browser: true,
     es2017: true,
