e167becc36dd5a761d467bfd1a68b903690ddf8a
nanderstabel <nander.stabel@impierce.com>
2023-10-02 19:03:37 +0800

    WIP

--- src-tauri/tests/README.md
+++ src-tauri/tests/README.md
@@ -1,10 +1,6 @@
 
 # Tests
 
-
-
-
-
 ```mermaid
 flowchart LR
     classDef action fill:#999,color:#000;
@@ -12,14 +8,26 @@ flowchart LR
     GetState2:::action
     CreateNew(CreateNew):::action
     UnlockStorage(UnlockStorage):::action
+    QrCodeScanned1(QrCodeScanned):::action
+    QrCodeScanned2(QrCodeScanned):::action
+    QrCodeScanned3(QrCodeScanned):::action
+    CredentialOffersSelected(CredentialOffersSelected):::action
+    ConnectionAccepted(ConnectionAccepted):::action
+    CredentialsSelected(CredentialsSelected):::action
 
     classDef payload fill:#CCC,color:#555,text-align:left;
-    P1("`name: Ferris Crabman
-        picture: 129408
-        theme: system
-        password: sup3rSecr3t
+    P1("`name: 'Ferris Crabman'
+        picture: '&#129408'
+        theme: 'system'
+        password: 'sup3rSecr3t'
     `"):::payload
-    P2("`password: sup3rSecr3t`"):::payload
+    P2("`password: 'sup3rSecr3t'`"):::payload
+    P3("`form_urlencoded: 'openid-credential-offer://?credential_offer=%7B%22credential_issuer%22%3A%22http%3A%2F%2F0.0.0.0%3A8000%2F%22%2C%22credentials%22%3A%5B%7B%22format%22%3A%22jwt_vc_json%22%2C%22credential_definition%22%3A%7B%22type%22%3A%5B%22VerifiableCredential%22%2C%22PersonalInformation%22%5D%7D%7D%5D%2C%22grants%22%3A%7B%22urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Apre-authorized_code%22%3A%7B%22pre-authorized_code%22%3A%220YI5DXtuCltKyNa5%22%2C%22user_pin_required%22%3Afalse%7D%7D%7D'`"):::payload
+    P4("`offer_indices: '[0]'`"):::payload
+    P5("`form_urlencoded: 'siopv2://idtoken?response_type=id_token&client_id=did%3Akey%3Az6Mkm9yeuZK7inXBNjnNH3vAs9uUjqfy3mfNoKBKsKBrv8Tb&scope=openid&redirect_uri=https%3A%2F%2Fexample.com&nonce=nonce'`"):::payload
+    P6("`form_urlencoded: 'siopv2://idtoken?response_type=vp_token&client_id=did%3Akey%3Az6Mkm9yeuZK7inXBNjnNH3vAs9uUjqfy3mfNoKBKsKBrv8Tb&presentation_definition=%7B%22id%22%3A%22Verifiable+Presentation+request+for+sign-on%22%2C%22input_descriptors%22%3A%5B%7B%22id%22%3A%22Request+for+Ferris%27s+Verifiable+Credential%22%2C%22constraints%22%3A%7B%22fields%22%3A%5B%7B%22path%22%3A%5B%22%24.vc.type%22%5D%2C%22filter%22%3A%7B%22type%22%3A%22array%22%2C%22contains%22%3A%7B%22const%22%3A%22PersonalInformation%22%7D%7D%7D%2C%7B%22path%22%3A%5B%22%24.vc.credentialSubject.givenName%22%5D%7D%2C%7B%22path%22%3A%5B%22%24.vc.credentialSubject.familyName%22%5D%7D%2C%7B%22path%22%3A%5B%22%24.vc.credentialSubject.email%22%5D%7D%2C%7B%22path%22%3A%5B%22%24.vc.credentialSubject.birthdate%22%5D%7D%5D%7D%7D%5D%7D&redirect_uri=https%3A%2F%2Fexample.com&nonce=nonce'`"):::payload
+    P7("`credentials selected: '[0]'`"):::payload
+
 
     subgraph o
     direction LR
@@ -29,6 +37,17 @@ flowchart LR
         C --- GetState2(GetState) --> D[[password_required.json]]
         D --- UnlockStorage --> E[[storage_unlocked.json]]
         P2 -.-> UnlockStorage
+        C --- QrCodeScanned1 --> F[[credential_offer_user_prompt.json]]
+        P3 -.-> QrCodeScanned1
+        F --- CredentialOffersSelected --> G[[new_credentials.json]]
+        P4 -.-> CredentialOffersSelected
+        C --- QrCodeScanned2 --> H[[siopv2_request_user_prompt.json]]
+        P5 -.-> QrCodeScanned2
+        H --- ConnectionAccepted --> I[[accepted_connection.json]]
+        G --- QrCodeScanned3 --> J[[oid4vp_response_user_prompt.json]]
+        P6 -.-> QrCodeScanned3
+        J --- CredentialsSelected --> K[[credentials_sent.json]]
+        P7 -.-> CredentialsSelected
     end
 
 ```
--- src-tauri/tests/tests/mod.rs
+++ src-tauri/tests/tests/mod.rs
@@ -1,3 +1,200 @@
 mod get_state;
 mod load_dev_profile;
 mod qr_code_scanned;
+
+///////////////////////////////////////
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Deref;
+
+    use crate::common::assert_state_update::{assert_state_update, setup_state_file, setup_stronghold};
+    use identity_wallet::state::{
+        actions::{Action, ActionType},
+        AppState,
+    };
+    pub use pest::Parser;
+    use pest_derive::Parser;
+
+    #[derive(Parser)]
+    #[grammar = "../src-tauri/flowchart.pest"]
+    pub struct MermaidParser;
+
+    #[tokio::test]
+    async fn test() {
+        print_mermaid().await;
+    }
+
+    async fn print_mermaid() -> anyhow::Result<()> {
+        use std::collections::HashMap;
+        let mut idents = HashMap::new();
+        let mut payloads = HashMap::new();
+        let mut actions = HashMap::new();
+
+        let unparsed_file = std::fs::read_to_string("1.mermaid").expect("cannot read file");
+
+        let file = MermaidParser::parse(Rule::file, &unparsed_file)
+            .unwrap()
+            .next()
+            .unwrap();
+
+        // println!("file: {:#?}", file);
+        println!("__________________________________________________________");
+        println!("__________________________________________________________");
+
+        let temp2 = file
+            .clone()
+            .into_inner()
+            .find(|pair| pair.as_rule() == Rule::body)
+            .map(|body| {
+                body.into_inner()
+                    .filter(|pair| pair.as_rule() == Rule::payload_assign)
+                    .for_each(|payload_assign| {
+                        let mut pair = payload_assign.into_inner().nth(0).unwrap().into_inner();
+
+                        let ident = pair.next().unwrap();
+                        println!("ident: {:#?}", ident.as_str());
+
+                        let vec: Vec<_> = pair
+                            .next()
+                            .unwrap()
+                            .into_inner()
+                            .nth(0)
+                            .unwrap()
+                            .into_inner()
+                            .map(|pair| pair.as_str())
+                            .collect();
+                        let string = vec.iter().cloned().collect::<Vec<&str>>().join("\n");
+                        let mapping: serde_json::Value = serde_yaml::from_str(&string).unwrap();
+                        println!("mapping: {:#?}", mapping);
+                        payloads.insert(ident.as_str(), mapping);
+                    })
+            });
+
+        let mut temp = file
+            .into_inner()
+            .find(|pair| pair.as_rule() == Rule::body)
+            .map(|body| {
+                body.into_inner()
+                    .filter(|pair| pair.as_rule() == Rule::subgraph)
+                    .map(|subgraph| {
+                        subgraph
+                            .into_inner()
+                            .find(|pair| pair.as_rule() == Rule::graph)
+                            .map(|graph| {
+                                graph
+                                    .into_inner()
+                                    .filter(|pair| pair.as_rule() == Rule::chain)
+                                    .filter_map(|chain| {
+                                        // println!("graph: {:#?}", chain);
+                                        // println!("__________________________________________________________");
+                                        // println!("__________________________________________________________");
+                                        let mut chain = chain.into_inner();
+                                        match (
+                                            chain.next(),
+                                            chain.next(),
+                                            chain.next(),
+                                            chain.next(),
+                                            chain.next(),
+                                            chain.next(),
+                                        ) {
+                                            (
+                                                Some(first),
+                                                Some(second),
+                                                Some(third),
+                                                Some(fourth),
+                                                Some(fifth),
+                                                None,
+                                            ) if first.as_rule() == Rule::ident_comb
+                                                && second.as_rule() == Rule::link
+                                                && third.as_rule() == Rule::ident_comb
+                                                && fourth.as_rule() == Rule::link
+                                                && fifth.as_rule() == Rule::ident_comb =>
+                                            {
+                                                let mut first = first.into_inner().next().unwrap().into_inner();
+                                                let (ident, path) = match (first.next(), first.next(), first.next()) {
+                                                    (Some(ident), Some(path), None) => {
+                                                        let path = path.into_inner().nth(0).unwrap().as_str();
+                                                        idents.insert(ident.as_str(), path);
+                                                        (ident.as_str(), path)
+                                                    }
+                                                    (Some(ident), None, None) => {
+                                                        let path = idents.get(ident.as_str()).unwrap();
+                                                        (ident.as_str(), *path)
+                                                    }
+                                                    _ => panic!("first"),
+                                                };
+
+                                                let link = PairWrapper(third).child(0)?.child(0)?.0.as_str();
+
+                                                // println!("action: {:#?}", link.as_str());
+
+                                                let mut fifth = fifth.into_inner().next().unwrap().into_inner();
+                                                let (ident2, path2) = match (fifth.next(), fifth.next(), fifth.next()) {
+                                                    (Some(ident), Some(path), None) => {
+                                                        let path = path.into_inner().nth(0).unwrap().as_str();
+                                                        idents.insert(ident.as_str(), path);
+                                                        (ident.as_str(), path)
+                                                    }
+                                                    (Some(ident), None, None) => {
+                                                        let path = idents.get(ident.as_str()).unwrap();
+                                                        (ident.as_str(), *path)
+                                                    }
+                                                    _ => panic!("fifth"),
+                                                };
+                                                // println!("state: {:#?} : {:#?}", ident2, path2);
+                                                Some((path, link, path2))
+                                            }
+                                            (Some(first), Some(second), Some(third), None, None, None)
+                                                if first.as_rule() == Rule::ident_comb
+                                                    && second.as_rule() == Rule::link
+                                                    && third.as_rule() == Rule::ident_comb =>
+                                            {
+                                                let ident = PairWrapper(first).child(0)?.child(0)?.0.as_str();
+
+                                                let payload = payloads.get(ident).unwrap();
+
+                                                actions.insert(third.as_str(), payload);
+                                                None
+                                            }
+                                            _ => panic!("third"),
+                                        }
+                                    })
+                                    .collect::<Vec<_>>()
+                            })
+                            .unwrap()
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .unwrap();
+
+        for (path, link, path2) in &temp[0] {
+            setup_state_file();
+            setup_stronghold();
+
+            let payload = actions.get(link);
+            println!("state: {:#?}", path);
+            println!("link: {:#?} {:#?}", link, payload);
+            println!("state2: {:#?}", path2);
+
+            assert_state_update(
+                AppState::default(),
+                vec![Action {
+                    r#type: ActionType::from_tag_str(*link),
+                    payload: payload.cloned().cloned(),
+                }],
+                vec![],
+            )
+            .await;
+        }
+
+        Ok(())
+    }
+    pub struct PairWrapper(pub pest::iterators::Pair<'static, Rule>);
+
+    impl PairWrapper {
+        pub fn child(self, index: u64) -> Option<Self> {
+            Some(Self(self.0.into_inner().nth(index as usize).unwrap()))
+        }
+    }
+}
