2ce11e800926f833fdae0206f6ae30cca2521e00
nanderstabel <nander.stabel@impierce.com>
2023-10-10 22:36:04 +0800

    feat: refactor backend

--- src-tauri/src/state/user_prompt.rs
+++ src-tauri/src/state/user_prompt.rs
@@ -1,104 +1,61 @@
+use oid4vci::credential_offer::CredentialOffer;
+use serde::{Deserialize, Serialize};
+use ts_rs::TS;
+
 /// "User prompts" are a way for the backend to communicate a desired/required user interaction to the frontend.
 /// This application design leaves it up to the frontend how it wants to handle such "user prompts".
 /// Having too much frontend logic in the backend would pollute the loose coupling and make it a lot harder to maintain.
 /// For example, the backend can "ask" the frontend to redirect the user to a specific page (e.g. the welcome page).
 /// Another example would be that the backend requires some user input to continue, but does not want to be specific about what
 /// is displayed to the user exactly.
-use serde::{Deserialize, Serialize};
-use ts_rs::TS;
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[ts(export, export_to = "bindings/user-prompt/Redirect.ts")]
-#[serde(deny_unknown_fields)]
-pub struct Redirect {
-    pub r#type: CurrentUserPromptType,
-    pub target: String,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[ts(export, export_to = "bindings/user-prompt/Warning.ts")]
-#[serde(deny_unknown_fields)]
-pub struct Warning {
-    pub r#type: CurrentUserPromptType,
-    pub message: String,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[ts(export, export_to = "bindings/user-prompt/Selection.ts")]
-#[serde(deny_unknown_fields)]
-pub struct Selection {
-    pub r#type: CurrentUserPromptType,
-    /// An option is in the form: (<option_name>, <option_value>)
-    // pub options: Vec<(String, String)>,
-    pub options: Vec<String>,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[ts(export, export_to = "bindings/user-prompt/CredentialOffer.ts")]
-#[serde(deny_unknown_fields)]
-pub struct CredentialOffer {
-    pub r#type: CurrentUserPromptType,
-    pub issuer_name: String,
-    pub logo_uri: Option<String>,
-    #[ts(type = "object")]
-    pub credential_offer: serde_json::Value,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[ts(export, export_to = "bindings/user-prompt/PasswordRequired.ts")]
-#[serde(deny_unknown_fields)]
-pub struct PasswordRequired {
-    pub r#type: CurrentUserPromptType,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[ts(export, export_to = "bindings/user-prompt/AcceptConnection.ts")]
-#[serde(deny_unknown_fields)]
-pub struct AcceptConnection {
-    pub r#type: CurrentUserPromptType,
-    pub client_name: String,
-    pub logo_uri: Option<String>,
-    pub redirect_uri: String,
-    pub previously_connected: bool,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[ts(export, export_to = "bindings/user-prompt/ShareCredentials.ts")]
-#[serde(deny_unknown_fields)]
-pub struct ShareCredentials {
-    pub r#type: CurrentUserPromptType,
-    pub client_name: String,
-    pub logo_uri: Option<String>,
-    pub options: Vec<String>,
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[serde(untagged)]
-#[ts(export, export_to = "bindings/user-prompt/CurrentUserPrompt.ts")]
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, TS)]
+#[serde(tag = "type")]
+#[ts(export)]
 pub enum CurrentUserPrompt {
-    Redirect(Redirect),
-    Warning(Warning),
-    Selection(Selection),
-    CredentialOffer(CredentialOffer),
-    PasswordRequired(PasswordRequired),
-    AcceptConnection(AcceptConnection),
-    ShareCredentials(ShareCredentials),
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, TS, PartialEq)]
-#[ts(export, export_to = "bindings/user-prompt/CurrentUserPromptType.ts")]
-pub enum CurrentUserPromptType {
     #[serde(rename = "redirect")]
-    Redirect,
-    // TODO: remove or rename generic warning
+    Redirect { target: String },
     #[serde(rename = "warning")]
-    Warning,
-    #[serde(rename = "accept-connection")] // SIOPv2
-    AcceptConnection,
-    #[serde(rename = "share-credentials")] // VP
-    ShareCredentials,
-    #[serde(rename = "credential-offer")]
-    CredentialOffer,
+    Warning { message: String },
+    #[serde(rename = "selection")]
+    Selection { options: Vec<String> },
     #[serde(rename = "password-required")]
     PasswordRequired,
+    #[serde(rename = "accept-connection")]
+    AcceptConnection {
+        client_name: String,
+        logo_uri: Option<String>,
+        redirect_uri: String,
+        previously_connected: bool,
+    },
+    #[serde(rename = "credential-offer")]
+    CredentialOffer {
+        issuer_name: String,
+        logo_uri: Option<String>,
+        #[ts(type = "object")]
+        credential_offer: CredentialOffer,
+    },
+    #[serde(rename = "share-credentials")]
+    ShareCredentials {
+        client_name: String,
+        logo_uri: Option<String>,
+        options: Vec<String>,
+    },
+}
+
+#[test]
+fn test() {
+    // let up = CurrentUserPrompt::CredentialOffer {
+    //     issuer_name: "test".into(),
+    //     logo_uri: None,
+    //     credential_offer: serde_json::json!({}),
+    // };
+
+    let up2 = CurrentUserPrompt::Redirect { target: "test".into() };
+
+    // let json = serde_json::to_string_pretty(&up).unwrap();
+    // println!("{}", json);
+    let json = serde_json::to_string_pretty(&up2).unwrap();
+    println!("{}", json);
+
+    // let cup: CurrentUserPrompt<PasswordRequired> = serde_json::from_str(&json).unwrap();
 }
