ab467eaf40f21c71a712f4acc376cd5b2f6e2bdf
Soula Andrikopoulos <soulaandrikopoulos@gmail.com>
2023-11-09 09:38:12 +0100

    test

--- src/lib/home-header/WelcomeMessage.svelte
+++ src/lib/home-header/WelcomeMessage.svelte
@@ -2,11 +2,21 @@
   import LL from '$src/i18n/i18n-svelte';
   import { state } from '$src/stores';
 
-  const greetings = ['Welcome to your UniMe', 'Hey', "What's up", 'How are you', 'Welcome back', 'Hello'];
+  export let blabla : string; 
+
+  const greetings = [
+    'Welcome to your UniMe',
+    'Hey',
+    "What's up",
+    'How are you',
+    'Welcome back',
+    'Hello'
+  ];
 </script>
 
 <p class="py-[10px] text-3xl font-semibold tracking-tight text-slate-700 dark:text-grey">
   <!-- {$LL.WELCOME()}, <span class="text-primary">{$state?.active_profile?.display_name}.</span> -->
   {greetings[Math.floor(Math.random() * greetings.length)]},
   <span class="text-primary">{$state?.active_profile?.name}.</span>
+  {blabla}
 </p>
--- src/routes/(app)/me/+page.svelte
+++ src/routes/(app)/me/+page.svelte
@@ -49,7 +49,7 @@
   </div>
 
   <div class="p-[18px] pt-0">
-    <WelcomeMessage />
+    <WelcomeMessage blabla="hello"/>
     {#if $state?.user_journey}
       <UserJourney />
     {/if}
@@ -60,6 +60,17 @@
     in:fly={{ y: 24, duration: 200 }}
     class="flex grow flex-col items-stretch justify-start rounded-t-[20px] bg-silver p-[18px] dark:bg-navy"
   >
+  <div class="search_bar">
+    <form>
+      <h3 class="text-gray-100 text-sm font-medium m-2">Search stuff here</h3>
+      <svg class="fixed m-3 mr-5 w-5 h-5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+        <path d="M21 21l-5.2-5.2"></path>
+        <path d="M15 11c0-3.9-3.1-7-7-7s-7 3.1-7 7 3.1 7 7 7 7-3.1 7-7"></path>
+    </svg>
+      <input type="text" class="rounded-full bg-white text-gray-600 px-4 py-3 text-sm w-11/12 ml-1 mb-6 pl-8" placeholder="abc.com...">
+  </form>
+  </div>
+
     {#if $state?.credentials && $state?.credentials.length > 0}
       <Favorites />
       <CredentialList />
