3d2358c0e7c47d31eb71b111548735e2e8f52477
Daniel Mader <daniel.mader@impierce.com>
2023-11-10 07:45:13 +0100

    refactor: inline tailwind css

--- src/lib/components/atoms/Switch.svelte
+++ src/lib/components/atoms/Switch.svelte
@@ -21,18 +21,10 @@
 
 <button
   use:melt={$root}
-  class="relative h-7 w-11 rounded-full bg-primary/25 transition-colors data-[state=checked]:bg-primary"
+  class="group relative h-7 w-11 rounded-full bg-primary/25 transition-colors data-[state=checked]:bg-primary"
 >
-  <span class="thumb m-0.5 block h-5 w-5 translate-x-0.5 rounded-full bg-white transition"></span>
+  <span
+    class="m-0.5 block h-5 w-5 translate-x-0.5 rounded-full bg-white transition group-data-[state=checked]:translate-x-[18px]"
+  ></span>
 </button>
 <input use:melt={$input} />
-
-<style>
-  .thumb {
-    /* TODO: refactor :global([...]) to inline tailwind in <span> */
-  }
-
-  :global([data-state='checked']) .thumb {
-    transform: translateX(calc(2.75rem - 1.25rem - 0.125rem - 4px));
-  }
-</style>
