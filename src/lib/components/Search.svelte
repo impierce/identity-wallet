da30030ba2e2d7074aee9a45768bd3cb861fa2b6
Soula Andrikopoulos <soulaandrikopoulos@gmail.com>
2023-11-10 15:50:44 +0100

    Connect frontend to backend

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,17 +1,31 @@
 <script lang="ts">
-//here I will put the import
-//probably an export..?
-</script>
+  import { dispatch } from '../dispatcher';
 
+  //here I will put the import
+  //probably an export..?
+</script>
 
 <!-- The HTML and TailwindCSS of the search component-->
 <div class="search_bar">
-    <form>
-      <h3 class="text-gray-100 text-sm font-medium m-2">Search stuff here</h3>
-      <svg class="fixed m-3 mr-5 w-5 h-5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
-        <path d="M21 21l-5.2-5.2"></path>
-        <path d="M15 11c0-3.9-3.1-7-7-7s-7 3.1-7 7 3.1 7 7 7 7-3.1 7-7"></path>
+  <form>
+    <h3 class="m-2 text-sm font-medium text-gray-100">Search stuff here</h3>
+    <svg
+      class="fixed m-3 mr-5 h-5 w-5"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      viewBox="0 0 24 24"
+    >
+      <path d="M21 21l-5.2-5.2"></path>
+      <path d="M15 11c0-3.9-3.1-7-7-7s-7 3.1-7 7 3.1 7 7 7 7-3.1 7-7"></path>
     </svg>
-      <input type="text" class="rounded-full bg-white text-gray-600 px-4 py-3 text-sm w-11/12 ml-1 mb-6 pl-8" placeholder="abc.com...">
+    <input
+      type="text"
+      class="mb-6 ml-1 w-11/12 rounded-full bg-white px-4 py-3 pl-8 text-sm text-gray-600"
+      placeholder="abc.com..."
+      on:input={(e) => dispatch({ type: '[User Data] Query', payload: { target: 'Credentials', search_term: e.target.value } })}
+    />
   </form>
-  </div>
+</div>
--- src/routes/(app)/me/+page.svelte
+++ src/routes/(app)/me/+page.svelte
@@ -27,6 +27,7 @@
   import RocketLaunch from '~icons/ph/rocket-launch-fill';
 
   import { calculate_initials } from './utils';
+  import Search from '$src/lib/components/Search.svelte';
 
   let initials: string | undefined;
 
@@ -60,16 +61,8 @@
     in:fly={{ y: 24, duration: 200 }}
     class="flex grow flex-col items-stretch justify-start rounded-t-[20px] bg-silver p-[18px] dark:bg-navy"
   >
-  <div class="search_bar">
-    <form>
-      <h3 class="text-gray-100 text-sm font-medium m-2">Search stuff here</h3>
-      <svg class="fixed m-3 mr-5 w-5 h-5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
-        <path d="M21 21l-5.2-5.2"></path>
-        <path d="M15 11c0-3.9-3.1-7-7-7s-7 3.1-7 7 3.1 7 7 7 7-3.1 7-7"></path>
-    </svg>
-      <input type="text" class="rounded-full bg-white text-gray-600 px-4 py-3 text-sm w-11/12 ml-1 mb-6 pl-8" placeholder="abc.com...">
-  </form>
-  </div>
+  <Search></Search>
+  
 
     {#if $state?.credentials && $state?.credentials.length > 0}
       <Favorites />
