49c56629b58e500aadbc417da0a5f4c2945726fb
Daniel Mader <daniel.mader@impierce.com>
2023-12-01 09:14:44 +0100

    fix: color adjustments

--- src/lib/components/ProgressBar.svelte
+++ src/lib/components/ProgressBar.svelte
@@ -4,7 +4,7 @@
 
 <!-- TODO: remove unused classes -->
 
-<div class="flex h-[10px] overflow-hidden rounded bg-grey dark:bg-navy">
+<div class="flex h-[10px] overflow-hidden rounded bg-grey dark:bg-blue">
   <div
     style="width:{value}%"
     class="flex flex-col justify-center whitespace-nowrap rounded-lg bg-primary text-center text-white shadow-none"
