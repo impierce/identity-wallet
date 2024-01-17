dd23bd866ef92b838a9de9373da41ab6de8ef681
Daniel Mader <daniel.mader@impierce.com>
2024-01-17 21:42:48 +0100

    ci: release version 0.4.7

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -24,11 +24,11 @@ android {
     namespace = "com.impierce.unime"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "com.impierce.unime"
+        applicationId = "com.impierce.identity_wallet"
         minSdk = 24
         targetSdk = 33
-        versionCode = 31
-        versionName = "0.4.6"
+        versionCode = 32
+        versionName = "0.4.7"
     }
     signingConfigs {
         create("release") {
