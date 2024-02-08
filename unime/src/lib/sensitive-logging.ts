90cceb85a4708a2912b64f567c1510eedd0efe05
Joris Willems <jwillems25@hotmail.com>
2024-02-08 15:36:13 +0100

    feat: prevent logging of sensitive data, fix: prevent app deadlocks (#106)
    * init new directories
    
    * new directory structure
    
    * Add gen-static directory
    
    This is needed when deploying to the stores. And now the init command
    can be safely used
    
    * implement feedback
    
    * add logging protection and deadlock protection
    
    * fix tests
    
    * fix sanitize logging
    
    * add clone for state
    
    * ran format
    
    * remove files
    
    * add tests for secure logging
    
    * add feedback
    
    * change text

---
+++ unime/src/lib/sensitive-logging.ts
@@ -0,0 +1,29 @@
+// Because logging of svelte can end up in Tauri when stringified. This function will provide some safety
+
+// Its from a package online, its just the keywords that are often used for sensitive data. Its not a bulletproof solution.
+const defaultBadWords = ['password', 'pass', 'token', 'auth', 'secret', 'passphrase', 'card'];
+
+const sanitizeStringifyRecursively = function (object: any, replacer: any, space: any): any {
+  if (object === null || object === undefined) {
+    return object;
+  }
+
+  for (const [key, value] of Object.entries(object)) {
+    if (typeof value === 'object') {
+      object[key] = sanitizeStringifyRecursively(value, replacer, space);
+    } else if (defaultBadWords.indexOf(key.toLowerCase()) !== -1) {
+      object[key] = '*****';
+    }
+  }
+
+  return object;
+};
+
+export const sanitizeStringify = function (value: any, replacer?: any, space?: string | number | undefined): string {
+  if (typeof value === 'object') {
+    const result = sanitizeStringifyRecursively({ ...value }, replacer, space);
+    return JSON.stringify(result, replacer, space);
+  } else {
+    return JSON.stringify(value, replacer, space);
+  }
+};
