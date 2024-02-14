8872554f9c1c78a75c7e1a536cb3a54e73592c42
Andrew Brough <andrew.brough@gmail.com>
2024-02-14 11:56:43 +0000

    Add British translations

--- unime/src/i18n/en-GB/index.ts
+++ unime/src/i18n/en-GB/index.ts
@@ -4,12 +4,12 @@ import en from '../en';
 import type { Translation } from '../i18n-types';
 
 const en_GB = extendDictionary(en, {
-  // ...(en as Translation),
-  // ONBOARDING: {
-  //   CUSTOMIZE: {
-  //     NAVBAR_TITLE: 'Customisation',
-  //   },
-  // },
+  ...(en as Translation),
+  ONBOARDING: {
+    CUSTOMIZE: {
+      NAVBAR_TITLE: 'Customisation',
+    },
+  },
 }) satisfies Translation;
 
 export default en_GB;
