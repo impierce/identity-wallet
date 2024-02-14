1b35ce977f6bc8934e34209da1b1f9951aaca230
Daniel Mader <daniel.mader@impierce.com>
2024-02-14 15:55:20 +0100

    fix: determine file extension by `Content-Type`

--- identity-wallet/src/utils.rs
+++ identity-wallet/src/utils.rs
@@ -16,9 +16,6 @@ use crate::{error::AppError, ASSETS_DIR};
 /// - max. file size: 2MB
 /// - supported file types: `.png`, `.svg`
 pub async fn download_asset(url: reqwest::Url, logo_type: LogoType, index: usize) -> Result<(), AppError> {
-    let file_name = url.path_segments().unwrap().last().unwrap();
-    let extension = file_name.split('.').last().unwrap();
-
     let assets_dir = ASSETS_DIR.lock().unwrap().as_path().to_owned();
     let tmp_dir = assets_dir.join("tmp");
 
@@ -27,19 +24,20 @@ pub async fn download_asset(url: reqwest::Url, logo_type: LogoType, index: usize
         std::fs::create_dir(&tmp_dir)?;
     }
 
-    // TODO: in batch offer, use format!("{}_{}.{}", logo_type, index, extension)
-    let mut file = File::create(tmp_dir.join(format!("{}_{}.{}", logo_type, index, extension)))?;
-
     let response = reqwest::get(url.clone()).await?;
 
-    if let Some(content_type) = response.headers().get("content-type") {
-        if !["image/png", "image/svg+xml"].contains(&content_type.to_str().unwrap()) {
-            warn!("content_type: {:?}", content_type);
-            return Err(AppError::DownloadAborted("content-type is not supported"));
-        }
-    } else {
-        return Err(AppError::DownloadAborted("content-type is not set"));
-    };
+    let file_extension = response
+        .headers()
+        .get("content-type")
+        .map(|header_value| match header_value.to_str().unwrap() {
+            "image/png" => Ok("png"),
+            "image/svg+xml" => Ok("svg"),
+            _ => {
+                warn!("content_type is not supported: {:?}", header_value);
+                Err(AppError::DownloadAborted("content-type is not supported"))
+            }
+        })
+        .unwrap_or(Err(AppError::DownloadAborted("content-type is not set")))?;
 
     let mut content = Cursor::new(response.bytes().await?);
 
@@ -47,6 +45,9 @@ pub async fn download_asset(url: reqwest::Url, logo_type: LogoType, index: usize
     if content.get_ref().len() > 1_024 * 1_024 * 2 {
         return Err(AppError::DownloadAborted("File size is bigger than 2MB"));
     }
+
+    // TODO: in batch offer, use format!("{}_{}.{}", logo_type, index, extension)
+    let mut file = File::create(tmp_dir.join(format!("{}_{}.{}", logo_type, index, file_extension)))?;
 
     copy(&mut content, &mut file)?;
 
@@ -60,3 +61,5 @@ pub enum LogoType {
     #[strum(serialize = "credential")]
     CredentialLogo,
 }
+
+// TODO: add tests
