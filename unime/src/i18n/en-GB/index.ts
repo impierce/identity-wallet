e7d72d1d334ddea543bb950f92ce5a60dd1382a6
Andrew Brough <andrew.brough@gmail.com>
2024-02-21 12:29:00 +0000

    Update index.ts with GB specific edits

--- unime/src/i18n/en-GB/index.ts
+++ unime/src/i18n/en-GB/index.ts
@@ -9,6 +9,21 @@ const en_GB = extendDictionary(en, {
     CUSTOMIZE: {
       NAVBAR_TITLE: 'Customisation',
     },
+    PLEDGE: {
+        DESCRIPTION:
+          'Your data belongs to you and only you decide who you share it with. Full stop. In fact, your data never even touches any of our systems - unless you opt-in to one of the cloud storage options.',
+      },
+    ITEM_2: {
+        TITLE: 'We will not add trackers',
+        DESCRIPTION:
+          "We do not track your actions behind the scenes. Full stop. Not for testing or any other reasons. That's our pledge. We also do not collect any anonymous device information or usage statistics. That decision makes developing the app a bit harder for us, but we believe it is the right decision.",
+      },
+    SKIP: {
+        TITLE: 'Skip customisation',
+      },
+    SETTINGS: {
+        FAVORITES: 'My favourites',
+      },
   },
 }) satisfies Translation;
 
