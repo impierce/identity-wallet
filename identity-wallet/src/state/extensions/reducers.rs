d831e2647f759826600d9a713f88e43fd0f65801
Oran Dan <oran-dan@hotmail.com>
2024-02-22 17:54:31 +0100

    resolved suggestions Norlock

--- identity-wallet/src/state/extensions/reducers.rs
+++ identity-wallet/src/state/extensions/reducers.rs
@@ -7,18 +7,14 @@ use crate::state::AppState;
 pub async fn test_feat_state(state: AppState, action: Action) -> Result<AppState, AppError> {
     if let Some(test_feat_state) = listen::<CustomExtensionTest>(action) {
         
-        let mut new_state = AppState {
-            ..state
-        };
+        let mut new_state = state.clone();
 
         new_state.extensions.insert("test".to_string(), Box::new(CustomExtension{
             name: "new".to_string(),
             value: if test_feat_state.test_bool { "new".to_string() } else { "old".to_string() }
         }));
 
-        return Ok(AppState {
-            ..new_state
-        });
+        return Ok(new_state);
     }
     Ok(state)
 }
