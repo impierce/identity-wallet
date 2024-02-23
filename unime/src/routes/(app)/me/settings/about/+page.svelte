1c2d75a326c3c91dc4e1386defef7af62d0ad889
Daniel Mader <daniel.mader@impierce.com>
2024-02-23 14:29:48 +0100

    ci: release version 0.4.12

--- unime/src/routes/(app)/me/settings/about/+page.svelte
+++ unime/src/routes/(app)/me/settings/about/+page.svelte
@@ -6,14 +6,14 @@
 </script>
 
 <TopNavBar on:back={() => history.back()} title={$LL.SETTINGS.SUPPORT.ABOUT.NAVBAR_TITLE()} />
-<div class="content-height flex flex-col bg-silver dark:bg-navy">
+<div class="content-height bg-silver dark:bg-navy flex flex-col">
   <div class="flex flex-col p-8">
     <!-- Footer -->
     <div
       class="flex flex-col items-center pt-4 text-[13px]/[24px] font-normal text-slate-500 opacity-50 dark:text-slate-300"
     >
       <!-- <div class="pb-4" /> -->
-      <div>0.4.11</div>
+      <div>0.4.12</div>
       <div class="flex items-center pb-4">
         <p>{$LL.SETTINGS.SUPPORT.ABOUT.BUILT_WITH()}</p>
         <Heart class="pl-1" />
