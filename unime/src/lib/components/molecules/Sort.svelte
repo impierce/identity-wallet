6add21c036153a3c05fe22df8572000fe3271b56
Soula Andrikopoulos <soula.andrikopoulos@impierce.com>
2024-03-05 12:05:34 +0100

    wip

--- unime/src/lib/components/molecules/Sort.svelte
+++ unime/src/lib/components/molecules/Sort.svelte
@@ -12,18 +12,15 @@
   import Grid from '~icons/ph/squares-four';
 
   import BottomDrawer from './dialogs/BottomDrawer.svelte';
+
+  let preferredView: 'list' | 'grid' = 'list';
+
+
 </script>
 
-<!--
-<div class="sort_wrapper">
-  <button class=" flex h-10 w-10 items-center justify-center rounded-xl bg-white dark:bg-dark dark:text-gray-100">
-    <Slider class="m-2.5 h-5 w-5"></Slider>
-  </button>
-</div>
--->
 
 <div class="sort_wrapper">
-  <BottomDrawer titleText="Sorting">
+  <BottomDrawer titleText={$LL.SORT.TITLE()}>
     <button
       slot="trigger"
       let:trigger
@@ -33,28 +30,38 @@
     >
     <div slot="content" class="w-full">
       <ul class="relative pb-5">
-        <li class="flex gap-2 py-3 dark:text-gray-100">
+        <li
+          on:click={() => (preferredView = 'list')}
+          class={`flex gap-2 border p-[10px] py-3 text-slate-800 dark:text-gray-100 ${
+            preferredView == 'list' ? 'rounded-lg border-grey bg-silver' : 'border-transparent'
+          }`}
+        >
           <List />
-          <p class="text-sm font-medium">List view</p>
+          <p class="text-sm font-medium">{$LL.SORT.PREFERENCES.LIST_VIEW()}</p>
         </li>
-        <li class="flex gap-2 py-3 dark:text-gray-100">
+        <li
+          on:click={() => (preferredView = 'grid')}
+          class={`flex gap-2 border p-[10px] py-3 text-slate-800 dark:text-gray-100 ${
+            preferredView == 'grid' ? 'rounded-lg border-grey bg-silver' : 'border-transparent'
+          }`}
+        >
           <Grid />
           <p class="text-sm font-medium">Grid view</p>
         </li>
 
-        <hr class="full-width fill-[#EFEFEF]" />
+        <hr class="full-width fill-[#efefef]" />
 
-        <li class="flex gap-2 py-3 dark:text-gray-100">
+        <li class="flex gap-2 pb-3 pt-5 text-slate-800 dark:text-gray-100">
           <Ascending />
           <p class="text-sm font-medium">Alphabetical</p>
         </li>
 
-        <li class="flex gap-2 py-3 dark:text-gray-100">
+        <li class="flex gap-2 py-3 text-slate-800 dark:text-gray-100">
           <Issued />
           <p class="text-sm font-medium">Date issued</p>
         </li>
 
-        <li class="flex gap-2 py-3 dark:text-gray-100">
+        <li class="flex gap-2 py-3 text-slate-800 dark:text-gray-100">
           <Added />
           <p class="text-sm font-medium">Date added</p>
         </li>
