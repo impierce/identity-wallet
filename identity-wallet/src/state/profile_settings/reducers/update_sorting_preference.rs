13c1b01ce1598f19a4b4a9f753e2cccaf1a02083
Oran Dan <oran-dan@hotmail.com>
2024-03-20 13:54:19 +0100

    feat: add update_sorting_preference

--- identity-wallet/src/state/profile_settings/reducers/update_sorting_preference.rs
+++ identity-wallet/src/state/profile_settings/reducers/update_sorting_preference.rs
@@ -1,7 +1,7 @@
 use crate::{
     error::AppError,
     state::{
-        actions::{listen, Action}, profile_settings::actions::update_sorting_preference::UpdateSortingPreference, AppState
+        actions::{listen, Action}, profile_settings::{actions::update_sorting_preference::UpdateSortingPreference, ProfileSettings}, AppState
     },
 };
 
@@ -9,13 +9,22 @@ use log::debug;
 
 pub async fn update_sorting_preference(state: AppState, action: Action) -> Result<AppState, AppError> {
     if let Some(update_sorting) = listen::<UpdateSortingPreference>(action) {
+        let mut sorting_preferences = state.profile_settings.sorting_preferences.clone().unwrap_or_default();
+
         if let Some(credential_sorting) = update_sorting.credential_sorting {
             debug!("Update credential sorting preference set to: `{:?}`", credential_sorting);
+            sorting_preferences.credential_sorting = Some(credential_sorting);
         }
-        if let Some(connection_sorting) = update_sorting.connection_sorting {
+        else if let Some(connection_sorting) = update_sorting.connection_sorting {
             debug!("Update connection sorting preference set to: `{:?}`", connection_sorting);
+            sorting_preferences.connection_sorting = Some(connection_sorting);
         }
-        return Ok(AppState {
+
+        return Ok( AppState {
+            profile_settings: ProfileSettings {
+                sorting_preferences: Some(sorting_preferences),
+                ..state.profile_settings
+            },
             ..state
         });
     }
