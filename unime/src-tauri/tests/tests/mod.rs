3a5f13f9413fbf5bee04274fa8b73a6cc5a0d6e0
Oran Dan <oran-dan@hotmail.com>
2024-03-21 00:05:35 +0100

    feat: start adding tests for sorting functionality

--- unime/src-tauri/tests/tests/mod.rs
+++ unime/src-tauri/tests/tests/mod.rs
@@ -5,3 +5,4 @@ mod get_state;
 mod load_dev_profile;
 mod qr_code_scanned;
 mod user_data_query;
+mod sorting;
---
+++ unime/src-tauri/tests/tests/sorting.rs
@@ -0,0 +1,15 @@
+// use crate::common::json_example;
+// use identity_wallet::state::{actions::Action, AppState, AppStateContainer};
+// use tokio::sync::Mutex;
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_credentials_sorting() {
+    todo!()
+}
+
+#[tokio::test]
+#[serial_test::serial]
+async fn test_connections_sorting() {
+    todo!()
+}
