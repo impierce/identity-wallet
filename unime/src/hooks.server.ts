7f08a6c9260ba2f46e7c9134a5de73b7975019e2
Thilo Maier <git@maier.asia>
2024-04-15 21:12:39 +0200

    Clean up hooks

--- unime/src/hooks.server.ts
+++ unime/src/hooks.server.ts
@@ -1,18 +1,25 @@
-import type { Handle, HandleServerError } from '@sveltejs/kit';
+import { dev } from '$app/environment';
 
-export const handle = (async ({ event, resolve }) => {
-  const response = await resolve(event);
-  return response;
-}) satisfies Handle;
+import type { HandleServerError } from '@sveltejs/kit';
+import { info } from '@tauri-apps/plugin-log';
 
-export const handleError: HandleServerError = async ({ error }) => {
+// TODO: Refactor after upgrade to SvelteKit v2.
+// `error.message` may contain sensitive information that should not be exposed.
+// In v2 you get a safe `message` prop: https://kit.svelte.dev/docs/hooks#shared-hooks-handleerror.
+
+export const handleError: HandleServerError = async ({ error, event }) => {
+  info(`hooks.server.ts (handleError): event.url: "${event.url}"`);
+
   const errorId = crypto.randomUUID();
 
-  // eslint-disable-next-line no-console
-  console.error(error);
+  if (dev) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
 
   return {
-    message: error.message,
+    // Use type assertion to access `message` property on unknown type.
+    message: (error as Error).message,
     errorId,
   };
 };
