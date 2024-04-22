fba2f23115b255260a2dc014832303f24eff82a0
Thilo Maier <git@maier.asia>
2024-04-22 13:54:24 +0200

    Add 3 more dependencies to Vite's optimizeDeps include

--- unime/vite.config.ts
+++ unime/vite.config.ts
@@ -61,6 +61,9 @@ export default defineConfig(async () => ({
       'typesafe-i18n/utils',
       'typesafe-i18n',
       'typesafe-i18n/detectors',
+      'date-fns',
+      'date-fns/locale',
+      '@tauri-apps/plugin-barcode-scanner',
     ],
   },
 }));
