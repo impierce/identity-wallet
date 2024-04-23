ef6e0ca6b9a1065d4a53d3b1aafaf798ec948a4d
Soula Andrikopoulos <soula.andrikopoulos@impierce.com>
2024-04-23 21:34:14 +0200

    refactor: update action payload (#187)
    * refactor: update action payload
    
    * fix: remove comments, update sorting preferences
    
    * refactor: use bindings
    
    * feat: use preferences from state

--- unime/src/lib/connections/sorting/SortingSheet.svelte
+++ unime/src/lib/connections/sorting/SortingSheet.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import type { CredentialSortMethod } from '@bindings/CredentialSortMethod';
   import { melt } from '@melt-ui/svelte';
 
   import LL from '$src/i18n/i18n-svelte';
@@ -6,24 +7,33 @@
   import SortPreferencesButton from '$src/lib/connections/sorting/SortingSheetButton.svelte';
   import { dispatch } from '$src/lib/dispatcher';
   import AlphabeticalOrder from '$src/lib/static/AlphabeticalOrder.svelte';
+  import { state } from '$src/stores';
 
   import Issued from '~icons/ph/calendar-check';
   import Added from '~icons/ph/calendar-plus';
   import Slider from '~icons/ph/sliders-horizontal';
 
-  //import Ascending from '~icons/ph/sort-ascending';
-  //import Grid from '~icons/ph/squares-four';
+  import ActionSheet from '../../components/molecules/dialogs/ActionSheet.svelte';
 
-  import BottomDrawer from '../../components/molecules/dialogs/BottomDrawer.svelte';
+  let method: CredentialSortMethod = $state.profile_settings.sorting_preferences.credentials.sort_method;
+  let reversed: boolean = $state.profile_settings.sorting_preferences.credentials.reverse;
 
-  let preferredView: 'list' | 'grid' = 'list';
-  let preferences: 'alphabetical' | 'issued' | 'added';
-  let sortingOrder: 'ascending' | 'descending';
+  function updateSortingPreference(credential_sorting: CredentialSortMethod) {
+    if (method == credential_sorting) {
+      reversed = !reversed;
+    } else {
+      method = credential_sorting;
+    }
+    dispatch({
+      type: '[Settings] Update Sorting Preference',
+      payload: { credential_sorting, reverse: reversed },
+    });
+  }
 </script>
 
 <!-- bottom drawer and sorting button-->
 <div class="sort_wrapper">
-  <BottomDrawer titleText={$LL.SORT.TITLE()}>
+  <ActionSheet titleText={$LL.SORT.TITLE()}>
     <button
       slot="trigger"
       let:trigger
@@ -34,109 +44,29 @@
     <!-- bottom drawer and list items with preferred view and sorting preferences-->
     <div slot="content" class="w-full">
       <div class="relative pb-5">
-        <!--
-  <SortPreferencesButton
-            icon={List}
-            label={$LL.SORT.PREFERENCES.LIST_VIEW()}
-            active={preferredView == 'list'}
-            on:click={() => (preferredView = 'list')}
-          />
-          <SortPreferencesButton
-            icon={Grid}
-            label={$LL.SORT.PREFERENCES.GRID_VIEW()}
-            active={preferredView == 'grid'}
-            on:click={() => (preferredView = 'grid')}
-          />
-  
-          <hr class="full-width fill-[#efefef]" />
-          -->
-
         <SortPreferencesButton
           icon={AlphabeticalOrder}
           label={$LL.SORT.PREFERENCES.ALPHABETICAL()}
-          active={preferences == 'alphabetical'}
-          {sortingOrder}
-          on:click={() => {
-            // Preference already selected
-            if (preferences == 'alphabetical') {
-              if (sortingOrder == 'descending') {
-                dispatch({
-                  type: '[User Data] Query',
-                  payload: { target: 'Credentials', sort_method: 'NameAZ', sort_reverse: true },
-                });
-                sortingOrder = 'ascending';
-              } else {
-                dispatch({ type: '[User Data] Query', payload: { target: 'Credentials', sort_method: 'NameAZ' } });
-                sortingOrder = 'descending';
-              }
-              // Preference not yet selected
-            } else {
-              dispatch({ type: '[User Data] Query', payload: { target: 'Credentials', sort_method: 'NameAZ' } });
-              preferences = 'alphabetical';
-              sortingOrder = 'descending';
-            }
-          }}
+          active={method == 'name_az'}
+          {reversed}
+          on:click={() => updateSortingPreference('name_az')}
         />
         <SortPreferencesButton
           icon={Issued}
           label={$LL.SORT.PREFERENCES.DATE_ISSUED()}
-          active={preferences == 'issued'}
-          {sortingOrder}
-          on:click={() => {
-            // Preference already selected
-            if (preferences == 'issued') {
-              if (sortingOrder == 'ascending') {
-                dispatch({
-                  type: '[User Data] Query',
-                  payload: { target: 'Credentials', sort_method: 'IssuanceNewOld', sort_reverse: true },
-                });
-                sortingOrder = 'descending';
-              } else {
-                dispatch({
-                  type: '[User Data] Query',
-                  payload: { target: 'Credentials', sort_method: 'IssuanceNewOld' },
-                });
-                sortingOrder = 'ascending';
-              }
-              // Preference not yet selected
-            } else {
-              dispatch({
-                type: '[User Data] Query',
-                payload: { target: 'Credentials', sort_method: 'IssuanceNewOld' },
-              });
-              preferences = 'issued';
-              sortingOrder = 'ascending';
-            }
-          }}
+          active={method == 'issue_date_new_old'}
+          {reversed}
+          on:click={() => updateSortingPreference('issue_date_new_old')}
         />
         <SortPreferencesButton
           icon={Added}
           label={$LL.SORT.PREFERENCES.DATE_ADDED()}
-          active={preferences == 'added'}
-          {sortingOrder}
-          on:click={() => {
-            // Preference already selected
-            if (preferences == 'added') {
-              if (sortingOrder == 'ascending') {
-                dispatch({
-                  type: '[User Data] Query',
-                  payload: { target: 'Credentials', sort_method: 'AddedNewOld' },
-                });
-                sortingOrder = 'descending';
-              } else {
-                dispatch({ type: '[User Data] Query', payload: { target: 'Credentials', sort_method: 'AddedNewOld' } });
-                sortingOrder = 'ascending';
-              }
-              // Preference not yet selected
-            } else {
-              dispatch({ type: '[User Data] Query', payload: { target: 'Credentials', sort_method: 'AddedNewOld' } });
-              preferences = 'added';
-              sortingOrder = 'ascending';
-            }
-          }}
+          active={method == 'added_date_new_old'}
+          {reversed}
+          on:click={() => updateSortingPreference('added_date_new_old')}
         />
       </div>
     </div>
     <Button variant="primary" slot="close" let:close trigger={close} label={$LL.CLOSE()} />
-  </BottomDrawer>
+  </ActionSheet>
 </div>
--- unime/src/lib/credentials/CredentialList.svelte
+++ unime/src/lib/credentials/CredentialList.svelte
@@ -3,8 +3,6 @@
 
   import { goto } from '$app/navigation';
 
-  import type { DisplayCredential } from '@bindings/display-credential/DisplayCredential';
-
   import LL from '$src/i18n/i18n-svelte';
   import IconMessage from '$src/lib/components/molecules/IconMessage.svelte';
   import ListItemCard from '$src/lib/components/molecules/ListItemCard.svelte';
@@ -14,7 +12,7 @@
 
   export let credentialType: 'all' | 'data' | 'badges' = 'all';
 
-  let credentials: DisplayCredential[] = [];
+  $: credentials = $state.credentials;
 
   onMount(async () => {
     credentials = $state.credentials.filter((c) => !c.metadata.is_favorite);
@@ -28,18 +26,20 @@
 
 {#if credentials?.length > 0}
   <div class="flex flex-col space-y-2">
-    {#each credentials as credential}
-      <ListItemCard
-        id={credential.id}
-        title={credential.display_name}
-        description={credential.issuer_name ?? credential.data.issuer?.name ?? credential.data.issuer}
-        type={credential.data.type.includes('OpenBadgeCredential') ? 'badge' : 'data'}
-        on:click={() =>
-          credential.data.type.includes('OpenBadgeCredential')
-            ? goto(`/badges/${credential.id}`)
-            : goto(`/credentials/${credential.id}`)}
-      ></ListItemCard>
-    {/each}
+    {#key credentials}
+      {#each credentials as credential}
+        <ListItemCard
+          id={credential.id}
+          title={credential.display_name}
+          description={credential.issuer_name ?? credential.data.issuer?.name ?? credential.data.issuer}
+          type={credential.data.type.includes('OpenBadgeCredential') ? 'badge' : 'data'}
+          on:click={() =>
+            credential.data.type.includes('OpenBadgeCredential')
+              ? goto(`/badges/${credential.id}`)
+              : goto(`/credentials/${credential.id}`)}
+        ></ListItemCard>
+      {/each}
+    {/key}
   </div>
 {:else if $state?.credentials?.length === 0}
   <!-- Only show "No credentials" when there's also no favorites -->
