7291ae58490434af30da5dc8069235517b459128
Soula Andrikopoulos <soula.andrikopoulos@impierce.com>
2024-04-25 14:13:03 +0200

    chore: add labels and translations

--- unime/src/lib/connections/sorting/SortingSheetButton.svelte
+++ unime/src/lib/connections/sorting/SortingSheetButton.svelte
@@ -3,9 +3,6 @@
 
   import type { SvelteHTMLElements } from 'svelte/elements';
 
-  import ArrowDown from '~icons/ph/arrow-down-bold';
-  import ArrowUp from '~icons/ph/arrow-up-bold';
-
   export let icon: typeof SvelteComponent<SvelteHTMLElements['svg']>;
   export let label: string;
   export let active: boolean = false;
@@ -15,19 +12,29 @@
 
 <button
   on:click={() => dispatch('click')}
-  class={`my-1 flex w-full gap-2 border p-[10px] text-slate-800 dark:text-grey ${
-    active ? 'rounded-lg border-grey bg-silver dark:border-slate-600 dark:bg-navy' : 'border-transparent'
+  class={`dark:text-grey my-1 flex w-full gap-2 border p-[10px] text-slate-800 ${
+    active ? 'border-grey bg-silver dark:bg-navy rounded-lg dark:border-slate-600' : 'border-transparent'
   }`}
 >
   <svelte:component this={icon} />
   <p class="text-sm font-medium">{label}</p>
   {#if active}
     <div class="absolute right-2">
-      {#if reversed}
-        <ArrowUp class="text-primary" />
-      {:else}
-        <ArrowDown class="text-primary" />
-      {/if}
+      <p class="text-primary text-sm font-medium">
+        {label === 'Alphabetical'
+          ? reversed
+            ? 'Z to A'
+            : 'A to Z'
+          : label === 'Date Issued'
+            ? reversed
+              ? 'Newest'
+              : 'Oldest'
+            : label === 'Date Added'
+              ? reversed
+                ? 'Oldest'
+                : 'Newest'
+              : ''}
+      </p>
     </div>
   {/if}
 </button>
