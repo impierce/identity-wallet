77989adace00b18417f608376a100a7ee793370b
Thilo Maier <thilo.maier@impierce.com>
2024-05-10 20:10:45 +0200

    fix: Change color of inactive bottom nav items (#220)
    * Change color of inactive bottom nav items
    
    * Change color of active icon to primary
    
    * Reduce indicator height from 4px to 2px

--- unime/src/lib/components/molecules/navigation/BottomNavBar.svelte
+++ unime/src/lib/components/molecules/navigation/BottomNavBar.svelte
@@ -16,25 +16,13 @@
 <div class="flex h-[64px] justify-evenly bg-white py-0 dark:bg-dark sm:justify-center sm:space-x-24">
   <!-- 1: Me -->
   <BottomNavItem label={$LL.ME.BOTTOM_NAVIGATION_TITLE()} active={active == 'me'} on:click={() => dispatch('me')}>
-    <!-- Currently, there is no better way of applying conditional Tailwind classes to a named slot inside a component. -->
-    <!-- <User
-        slot="icon"
-        class={`h-6 w-6 ${
-          active == 'me' ? 'text-black dark:text-primary' : 'text-slate-400 dark:text-white'
-        }`}
-      /> -->
-    <Me
-      slot="icon"
-      class={`h-6 w-6 ${active == 'me' ? 'text-slate-800 dark:text-grey' : 'text-slate-300 dark:text-slate-500'}`}
-    />
+    <!-- Icons bring their own dimensions (height and width).  -->
+    <Me slot="icon" class="h-6 w-6" />
   </BottomNavItem>
 
   <!-- 2: Scan -->
   <BottomNavItem label={$LL.SCAN.BOTTOM_NAVIGATION_TITLE()} active={active == 'scan'} on:click={() => dispatch('scan')}>
-    <Scan
-      slot="icon"
-      class={`h-6 w-6 ${active == 'scan' ? 'text-slate-800 dark:text-grey' : 'text-slate-300 dark:text-slate-500'}`}
-    />
+    <Scan slot="icon" class="h-6 w-6" />
   </BottomNavItem>
 
   <!-- 3: Activity -->
@@ -43,9 +31,6 @@
     active={active == 'activity'}
     on:click={() => dispatch('activity')}
   >
-    <Activity
-      slot="icon"
-      class={`h-6 w-6 ${active == 'activity' ? 'text-slate-800 dark:text-grey' : 'text-slate-300 dark:text-slate-500'}`}
-    />
+    <Activity slot="icon" class="h-6 w-6" />
   </BottomNavItem>
 </div>
