51f5a1d9858a6da5cab34cdcc32ad817df0f3861
Thilo Maier <thilo.maier@impierce.com>
2024-05-15 15:35:33 +0200

    fix: turn ThemeSelect into a Melt UI radio group Fixes #229

--- unime/src/lib/app/settings/ThemeSelect.svelte
+++ unime/src/lib/app/settings/ThemeSelect.svelte
@@ -6,12 +6,10 @@
   import { createRadioGroup, melt } from '@melt-ui/svelte';
   import { debug } from '@tauri-apps/plugin-log';
 
-  import Checkbox from '$lib/components/atoms/Checkbox.svelte';
-
   export let defaultValue = 'system';
 
   const {
-    elements: { item },
+    elements: { root, item },
     helpers: { isChecked },
     states: { value },
   } = createRadioGroup({
@@ -44,58 +42,57 @@
   }
 </script>
 
-<!-- System -->
-<div class="mt-4 flex flex-col space-y-4">
-  <div
-    use:melt={$item('system')}
-    class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-slate-50 p-4 dark:border-slate-600
+<div use:melt={$root}>
+  <!-- System theme radio button -->
+  <div class="mt-4 flex flex-col space-y-4">
+    <div
+      use:melt={$item('system')}
+      class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-slate-50 p-4 dark:border-slate-600
         {$isChecked('system') ? 'ring ring-primary' : ''}"
-  >
-    <div class="h-12 grow">
-      <p class="text-[13px]/[24px] font-medium text-slate-800">{$LL.SETTINGS.THEME.SYSTEM()}</p>
-    </div>
-    <Checkbox checked={$isChecked('system')} />
-    <!-- Light -->
-    <div class="absolute bottom-0 left-1/4 h-14 w-1/4 rounded-t-xl border border-b-0 border-slate-200 bg-white">
-      <p class="p-2 text-sm font-medium text-slate-800">Aa</p>
-    </div>
-    <!-- Dark -->
-    <div class="absolute bottom-0 left-1/2 h-14 w-1/4 rounded-t-xl bg-slate-500">
-      <p class="p-2 text-sm font-medium text-white">Aa</p>
+    >
+      <div class="h-12 grow">
+        <p class="text-[13px]/[24px] font-medium text-slate-800">{$LL.SETTINGS.THEME.SYSTEM()}</p>
+      </div>
+      <!-- Light -->
+      <div class="absolute bottom-0 left-1/4 h-14 w-1/4 rounded-t-xl border border-b-0 border-slate-200 bg-white">
+        <p class="p-2 text-sm font-medium text-slate-800">Aa</p>
+      </div>
+      <!-- Dark -->
+      <div class="absolute bottom-0 left-1/2 h-14 w-1/4 rounded-t-xl bg-slate-500">
+        <p class="p-2 text-sm font-medium text-white">Aa</p>
+      </div>
     </div>
   </div>
-</div>
 
-<!-- Light -->
-<div class="mt-4 flex flex-col space-y-4">
-  <div
-    use:melt={$item('light')}
-    class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-white p-4 dark:border-slate-600
+  <!-- Light theme radio button -->
+  <div class="mt-4 flex flex-col space-y-4">
+    <div
+      use:melt={$item('light')}
+      class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-white p-4 dark:border-slate-600
         {$isChecked('light') ? 'ring ring-primary' : ''}"
-  >
-    <div class="h-12 grow">
-      <p class="text-[13px]/[24px] font-medium text-slate-800">{$LL.SETTINGS.THEME.LIGHT()}</p>
-    </div>
-    <Checkbox checked={$isChecked('light')} />
-    <div class="absolute bottom-0 left-1/4 h-14 w-1/2 rounded-t-xl border border-b-0 border-slate-200 bg-slate-100">
-      <p class="p-2 text-sm font-medium text-slate-800">Aa</p>
+    >
+      <div class="h-12 grow">
+        <p class="text-[13px]/[24px] font-medium text-slate-800">{$LL.SETTINGS.THEME.LIGHT()}</p>
+      </div>
+      <div class="absolute bottom-0 left-1/4 h-14 w-1/2 rounded-t-xl border border-b-0 border-slate-200 bg-slate-100">
+        <p class="p-2 text-sm font-medium text-slate-800">Aa</p>
+      </div>
     </div>
   </div>
-</div>
 
-<!-- Dark -->
-<div class="mt-4 flex flex-col space-y-4">
-  <div
-    use:melt={$item('dark')}
-    class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-slate-800 p-4 dark:border-slate-600
+  <!-- Dark theme radio button -->
+  <div class="mt-4 flex flex-col space-y-4">
+    <div
+      use:melt={$item('dark')}
+      class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-slate-800 p-4 dark:border-slate-600
         {$isChecked('dark') ? 'ring ring-primary' : ''}"
-  >
-    <div class="h-12 grow">
-      <p class="text-[13px]/[24px] font-medium text-white">{$LL.SETTINGS.THEME.DARK()}</p>
-    </div>
-    <Checkbox checked={$isChecked('dark')} />
-    <div class="absolute bottom-0 left-1/4 h-14 w-1/2 rounded-t-xl bg-slate-600">
-      <p class="p-2 text-sm font-medium text-white">Aa</p>
+    >
+      <div class="h-12 grow">
+        <p class="text-[13px]/[24px] font-medium text-white">{$LL.SETTINGS.THEME.DARK()}</p>
+      </div>
+      <div class="absolute bottom-0 left-1/4 h-14 w-1/2 rounded-t-xl bg-slate-600">
+        <p class="p-2 text-sm font-medium text-white">Aa</p>
+      </div>
     </div>
   </div>
 </div>
