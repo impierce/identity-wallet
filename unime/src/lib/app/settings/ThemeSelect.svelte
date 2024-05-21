ebedd79ec5eb1efefa5e4f7b61e231217583e3b8
Daniel Mader <daniel.mader@impierce.com>
2024-05-21 17:08:08 +0200

    fix: adjust styling to match designs

--- unime/src/lib/app/settings/ThemeSelect.svelte
+++ unime/src/lib/app/settings/ThemeSelect.svelte
@@ -42,57 +42,51 @@
   }
 </script>
 
-<div use:melt={$root}>
+<div use:melt={$root} class="flex flex-col gap-4">
   <!-- System theme radio button -->
-  <div class="mt-4 flex flex-col space-y-4">
-    <div
-      use:melt={$item('system')}
-      class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-slate-50 p-4 dark:border-slate-600
-        {$isChecked('system') ? 'ring ring-primary' : ''}"
-    >
-      <div class="h-12 grow">
-        <p class="text-[13px]/[24px] font-medium text-slate-800">{$LL.SETTINGS.THEME.SYSTEM()}</p>
-      </div>
-      <!-- Light -->
-      <div class="absolute bottom-0 left-1/4 h-14 w-1/4 rounded-t-xl border border-b-0 border-slate-200 bg-white">
-        <p class="p-2 text-sm font-medium text-slate-800">Aa</p>
-      </div>
-      <!-- Dark -->
-      <div class="absolute bottom-0 left-1/2 h-14 w-1/4 rounded-t-xl bg-slate-500">
-        <p class="p-2 text-sm font-medium text-white">Aa</p>
-      </div>
+  <div
+    use:melt={$item('system')}
+    class="relative flex items-center justify-between rounded-xl border bg-slate-50 p-4
+        {$isChecked('system') ? 'border-primary ring-1 ring-primary' : 'border-slate-200 dark:border-slate-600'}"
+  >
+    <div class="h-12 grow">
+      <p class="text-[13px]/[24px] font-medium text-slate-800">{$LL.SETTINGS.THEME.SYSTEM()}</p>
+    </div>
+    <!-- Light -->
+    <div class="absolute bottom-0 left-1/4 h-14 w-1/4 rounded-t-xl border border-b-0 border-slate-200 bg-white">
+      <p class="p-2 text-sm font-medium text-slate-800">Aa</p>
+    </div>
+    <!-- Dark -->
+    <div class="absolute bottom-0 left-1/2 h-14 w-1/4 rounded-t-xl border-b-0 border-slate-200 bg-blue">
+      <p class="p-2 text-sm font-medium text-white">Aa</p>
     </div>
   </div>
 
   <!-- Light theme radio button -->
-  <div class="mt-4 flex flex-col space-y-4">
-    <div
-      use:melt={$item('light')}
-      class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-white p-4 dark:border-slate-600
-        {$isChecked('light') ? 'ring ring-primary' : ''}"
-    >
-      <div class="h-12 grow">
-        <p class="text-[13px]/[24px] font-medium text-slate-800">{$LL.SETTINGS.THEME.LIGHT()}</p>
-      </div>
-      <div class="absolute bottom-0 left-1/4 h-14 w-1/2 rounded-t-xl border border-b-0 border-slate-200 bg-slate-100">
-        <p class="p-2 text-sm font-medium text-slate-800">Aa</p>
-      </div>
+  <div
+    use:melt={$item('light')}
+    class="relative flex items-center justify-between rounded-xl border bg-white p-4
+        {$isChecked('light') ? 'border-primary ring-1 ring-primary' : 'border-slate-200 dark:border-slate-600'}"
+  >
+    <div class="h-12 grow">
+      <p class="text-[13px]/[24px] font-medium text-slate-800">{$LL.SETTINGS.THEME.LIGHT()}</p>
+    </div>
+    <div class="absolute bottom-0 left-1/4 h-14 w-1/2 rounded-t-xl border border-b-0 border-slate-200 bg-slate-100">
+      <p class="p-2 text-sm font-medium text-slate-800">Aa</p>
     </div>
   </div>
 
   <!-- Dark theme radio button -->
-  <div class="mt-4 flex flex-col space-y-4">
-    <div
-      use:melt={$item('dark')}
-      class="relative flex items-center justify-between rounded-xl border border-slate-200 bg-slate-800 p-4 dark:border-slate-600
-        {$isChecked('dark') ? 'ring ring-primary' : ''}"
-    >
-      <div class="h-12 grow">
-        <p class="text-[13px]/[24px] font-medium text-white">{$LL.SETTINGS.THEME.DARK()}</p>
-      </div>
-      <div class="absolute bottom-0 left-1/4 h-14 w-1/2 rounded-t-xl bg-slate-600">
-        <p class="p-2 text-sm font-medium text-white">Aa</p>
-      </div>
+  <div
+    use:melt={$item('dark')}
+    class="relative flex items-center justify-between rounded-xl border bg-blue p-4
+        {$isChecked('dark') ? 'border-primary ring-1 ring-primary' : 'border-slate-200 dark:border-slate-600'}"
+  >
+    <div class="h-12 grow">
+      <p class="text-[13px]/[24px] font-medium text-white">{$LL.SETTINGS.THEME.DARK()}</p>
+    </div>
+    <div class="absolute bottom-0 left-1/4 h-14 w-1/2 rounded-t-xl bg-slate-600">
+      <p class="p-2 text-sm font-medium text-white">Aa</p>
     </div>
   </div>
 </div>
