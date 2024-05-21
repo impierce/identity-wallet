61f39a42b50eda20ca4165d755536cd0c44b6fa4
Nander Stabel <nander.stabel@impierce.com>
2024-05-21 16:24:31 +0200

    fix: fix `client_metadata`

--- identity-wallet/src/state/credentials/reducers/handle_oid4vp_authorization_request.rs
+++ identity-wallet/src/state/credentials/reducers/handle_oid4vp_authorization_request.rs
@@ -184,21 +184,17 @@ pub fn get_oid4vp_client_name_and_logo_uri(
     let client_id = oid4vp_authorization_request.body.client_id.clone();
 
     // Get the client_name and logo_uri from the client_metadata if it exists.
-    oid4vp_authorization_request
-        .body
-        .extension
-        .client_metadata
-        .as_ref()
-        .and_then(|client_metadata| match client_metadata {
-            ClientMetadataResource::ClientMetadata {
-                client_name, logo_uri, ..
-            } => {
-                let client_name = client_name.as_ref().cloned().unwrap_or(connection_url.to_string());
-                let logo_uri = logo_uri.as_ref().map(|logo_uri| logo_uri.to_string());
-                Some((client_name, logo_uri, connection_url.to_string(), client_id.clone()))
-            }
-            _ => None,
-        })
-        // Otherwise use the connection_url as the client_name.
-        .unwrap_or((connection_url.to_string(), None, connection_url.to_string(), client_id))
+    match &oid4vp_authorization_request.body.extension.client_metadata {
+        ClientMetadataResource::ClientMetadata {
+            client_name, logo_uri, ..
+        } => {
+            let client_name = client_name.as_ref().cloned().unwrap_or(connection_url.to_string());
+            let logo_uri = logo_uri.as_ref().map(|logo_uri| logo_uri.to_string());
+            Some((client_name, logo_uri, connection_url.to_string(), client_id.clone()))
+        }
+        // TODO: support `client_metadata_uri`
+        ClientMetadataResource::ClientMetadataUri(_) => None,
+    }
+    // Otherwise use the connection_url as the client_name.
+    .unwrap_or((connection_url.to_string(), None, connection_url.to_string(), client_id))
 }
