49198887a7c8de1235cc6515906d9aabcba5be7c
Thilo Maier <thilo.maier@impierce.com>
2024-07-11 17:50:39 +0200

    Update ESLint to v9

---
+++ unime/eslint.config.js
@@ -0,0 +1,40 @@
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import ts from 'typescript-eslint';
+
+import js from '@eslint/js';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser,
+      },
+    },
+  },
+  {
+    ignores: ['build/', '.svelte-kit/', 'dist/', 'src/i18n/*.ts'],
+  },
+  {
+    rules: {
+      'svelte/no-at-html-tags': 'warn', // TODO: security risk even applicable for context of Tauri app?
+      'no-console': 'error',
+    },
+  },
+];
