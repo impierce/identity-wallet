931c9fa89604d2dc9721d4b0a86eb3b66f91b2ca
Nander Stabel <nander.stabel@impierce.com>
2024-07-24 19:56:42 +0200

    fix: update supported_subject_syntax_types with preferred_did_methods

--- identity-wallet/src/state/did/reducers/preferred_method.rs
+++ identity-wallet/src/state/did/reducers/preferred_method.rs
@@ -1,4 +1,7 @@
+use std::str::FromStr;
+
 use log::debug;
+use oid4vc::oid4vc_core::SubjectSyntaxType;
 
 use crate::{
     error::AppError,
@@ -27,6 +30,24 @@ pub async fn set_preferred_did_method(state: AppState, action: Action) -> Result
 
         debug!("Order of preferred DID methods (updated): {:?}", preferred_did_methods);
 
+        let mut state_guard = state.core_utils.managers.lock().await;
+
+        let identity_manager = state_guard.identity_manager.as_mut().unwrap();
+
+        identity_manager
+            .provider_manager
+            .provider
+            .supported_subject_syntax_types = preferred_did_methods
+            .iter()
+            .map(|m| SubjectSyntaxType::from_str(m).unwrap())
+            .collect();
+
+        identity_manager.wallet.supported_subject_syntax_types = preferred_did_methods
+            .iter()
+            .map(|m| SubjectSyntaxType::from_str(m).unwrap())
+            .collect();
+
+        drop(state_guard);
         return Ok(AppState {
             profile_settings: ProfileSettings {
                 preferred_did_methods,
