faf2580e88d0fd8bc21358b2fe7e1cc97c79cd83
Oran Dan <oran-dan@hotmail.com>
2024-08-22 17:03:52 +0200

    test: add testing, not final

--- identity-wallet/src/state/trust_list/reducers/trust_list_reset.rs
+++ identity-wallet/src/state/trust_list/reducers/trust_list_reset.rs
@@ -8,10 +8,20 @@ use crate::state::{
 
 pub async fn trust_list_reset(state: AppState, action: Action) -> Result<AppState, AppError> {
     if let Some(_action) = listen::<TrustListReset>(action) {
-        let default_trust_list_file = std::fs::File::open(ASSETS_DIR.lock().unwrap().join("default_trust_list.json"))
-            .expect("error: default_trust_list.json not found");
-        let trust_list = serde_json::from_reader(default_trust_list_file)
-            .expect("error: failed to deserialize default_trust_list.json");
+        let trust_list: std::collections::HashMap<String, bool>;
+        
+        if let Ok(default_trust_list_file) =
+            std::fs::File::open(ASSETS_DIR.lock().unwrap().join("default_trust_list.json"))
+        {
+            trust_list = serde_json::from_reader(default_trust_list_file)
+                .expect("error: failed to deserialize default_trust_list.json");
+        } else { // strictly for testing purposes because ASSETS_DIR is not available in unit tests
+            println!("{}", std::env::current_dir().unwrap().to_str().unwrap());
+            let path = std::fs::File::open("resources/default_trust_list.json")
+                .expect("error: default_trust_list.json not found");
+            trust_list = serde_json::from_reader(path).expect("error: failed to deserialize default_trust_list.json");
+        }
+        // .expect("error: default_trust_list.json not found");
 
         return Ok(AppState {
             trust_list,
@@ -20,4 +30,26 @@ pub async fn trust_list_reset(state: AppState, action: Action) -> Result<AppStat
         });
     }
     Ok(state)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::state::trust_list::actions::trust_list_reset::TrustListReset;
+    use std::{collections::HashMap, sync::Arc};
+
+    #[tokio::test]
+    async fn test_trust_list_reset() {
+        let mut state = AppState::default();
+        state.trust_list.insert("https://www.example.com".to_string(), false);
+
+        let action = Arc::new(TrustListReset {});
+
+        let result = trust_list_reset(state, action).await.unwrap();
+
+        assert_eq!(
+            result.trust_list,
+            HashMap::from([("https://www.impierce.com".to_string(), true)])
+        );
+    }
 }
