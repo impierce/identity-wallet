fe3f971f4a56723419959aab0148fbdd9e568b99
Daniel Mader <daniel.mader@impierce.com>
2024-09-18 15:28:55 +0200

    fix: contain image within fixed height

--- unime/src/routes/credentials/[id]/DataUrlImageRenderer.svelte
+++ unime/src/routes/credentials/[id]/DataUrlImageRenderer.svelte
@@ -7,7 +7,7 @@
 @component
 This renderer for `credentialSubject` fields renders an embedded image for a given [Data URL](https://developer.mozilla.org/en-US/docs/Web/URI/Schemes/data).
 -->
-<div class="flex flex-col items-center rounded-xl bg-background-alt px-4 py-3 text-[13px]/[24px]">
+<div class="flex flex-col items-start rounded-xl bg-background px-4 py-3 text-[13px]/[24px]">
   <h2 class="w-full font-medium text-text-alt">{key}</h2>
-  <img src={dataUrl} alt="embedded" class="h-auto w-32 rounded-lg" />
+  <img src={dataUrl} alt="embedded" class="my-1 h-32 rounded-md object-contain" />
 </div>
