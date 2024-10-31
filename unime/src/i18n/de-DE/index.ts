2bfd1b6a516e2ab7158b08ee87f8bc57960d2363
Oran Dan <53859531+Oran-Dan@users.noreply.github.com>
2024-10-31 13:31:47 +0100

    feat: display `alignment` & `criteria` for Open Badges 3.0 (#386)
    * ci: release version 0.6.12 (#370)
    
    * chore(deps-dev): bump globals from 15.9.0 to 15.11.0 (#389)
    
    Bumps [globals](https://github.com/sindresorhus/globals) from 15.9.0 to 15.11.0.
    - [Release notes](https://github.com/sindresorhus/globals/releases)
    - [Commits](https://github.com/sindresorhus/globals/compare/v15.9.0...v15.11.0)
    
    ---
    updated-dependencies:
    - dependency-name: globals
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps-dev): bump typescript-eslint from 8.2.0 to 8.12.0 (#391)
    
    Bumps [typescript-eslint](https://github.com/typescript-eslint/typescript-eslint/tree/HEAD/packages/typescript-eslint) from 8.2.0 to 8.12.0.
    - [Release notes](https://github.com/typescript-eslint/typescript-eslint/releases)
    - [Changelog](https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/typescript-eslint/CHANGELOG.md)
    - [Commits](https://github.com/typescript-eslint/typescript-eslint/commits/v8.12.0/packages/typescript-eslint)
    
    ---
    updated-dependencies:
    - dependency-name: typescript-eslint
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps-dev): bump prettier-plugin-svelte from 3.2.6 to 3.2.7 (#382)
    
    Bumps [prettier-plugin-svelte](https://github.com/sveltejs/prettier-plugin-svelte) from 3.2.6 to 3.2.7.
    - [Changelog](https://github.com/sveltejs/prettier-plugin-svelte/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/sveltejs/prettier-plugin-svelte/compare/v3.2.6...v3.2.7)
    
    ---
    updated-dependencies:
    - dependency-name: prettier-plugin-svelte
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(deps-dev): bump postcss from 8.4.45 to 8.4.47 (#375)
    
    Bumps [postcss](https://github.com/postcss/postcss) from 8.4.45 to 8.4.47.
    - [Release notes](https://github.com/postcss/postcss/releases)
    - [Changelog](https://github.com/postcss/postcss/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/postcss/postcss/compare/8.4.45...8.4.47)
    
    ---
    updated-dependencies:
    - dependency-name: postcss
      dependency-type: direct:development
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore: resolve PR comments
    
    * chore: resole PR comment
    
    * chore: resolve PR comment
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Daniel Mader <daniel.mader@impierce.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- unime/src/i18n/de-DE/index.ts
+++ unime/src/i18n/de-DE/index.ts
@@ -267,9 +267,11 @@ const de = {
     NAVBAR_TITLE: 'Credential Informationen',
     DETAILS: {
       VALID: 'Gültig',
-      ISSUED_BY: 'Ausgestellt von',
+      ISSUED_BY: 'Ausgestellt durch',
       DESCRIPTION: 'Beschreibung',
-      CRITERIA: 'Criteria',
+      OPEN_BADGES: {
+        CRITERIA: 'Kriterien',
+      },
     },
     ACTIONS: {
       DELETE: {
