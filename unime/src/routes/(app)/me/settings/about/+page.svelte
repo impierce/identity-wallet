f7f0684843dde96e2d18ae7ed7f4b946f39ced25
Daniel Mader <daniel.mader@impierce.com>
2025-02-19 16:38:28 +0100

    ci: release version 0.8.0

--- unime/src/routes/(app)/me/settings/about/+page.svelte
+++ unime/src/routes/(app)/me/settings/about/+page.svelte
@@ -37,7 +37,7 @@
     {/if}
     <section class="mb-4 flex flex-col items-center">
       <h2 class="font-bold">{$LL.SETTINGS.SUPPORT.ABOUT.VERSION()}</h2>
-      <div class="mb-3">0.6.12</div>
+      <div class="mb-3">0.8.0</div>
       <div class="flex items-center">
         <p>{$LL.SETTINGS.SUPPORT.ABOUT.BUILT_WITH()}</p>
         <HeartFillIcon class="pl-1" />
