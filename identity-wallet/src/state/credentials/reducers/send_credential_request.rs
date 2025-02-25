5c0c0b9421a3b208caf6ba5e5a14809dc50faf71
Oran Dan <oran-dan@hotmail.com>
2025-02-25 17:44:08 +0100

    chore: save

--- identity-wallet/src/state/credentials/reducers/send_credential_request.rs
+++ identity-wallet/src/state/credentials/reducers/send_credential_request.rs
@@ -4,8 +4,7 @@ use crate::{
     state::{
         actions::{listen, Action},
         core_utils::{
-            history_event::{EventType, HistoryCredential, HistoryEvent},
-            CoreUtils,
+            helpers::get_unverified_jwt_claims, history_event::{EventType, HistoryCredential, HistoryEvent}, CoreUtils
         },
         credentials::{
             actions::credential_offers_selected::CredentialOffersSelected, DisplayCredential,
@@ -16,7 +15,8 @@ use crate::{
     },
 };
 
-use log::info;
+use identity_iota::core::ToJson;
+use log::{info, warn};
 use oid4vc::oid4vci::{
     credential_issuer::credential_configurations_supported::CredentialConfigurationsSupportedObject,
     credential_offer::Grants, credential_response::CredentialResponseType, token_request::TokenRequest,
@@ -159,6 +159,10 @@ pub async fn send_credential_request(state: AppState, action: Action) -> Result<
                     };
 
                     // TODO: Validate credential against its corresponding credential configuration.
+                    warn!("credential: {:?}", credential);
+                    warn!("credential_configuration: {:?}", credential_configuration);
+                    warn!("credential_configuration_json: {:?}", credential_configuration.to_json_value());
+                    validate_credential_configuration(&credential, &credential_configuration)?;
 
                     vec![(
                         credential_configuration_id,
@@ -296,6 +300,17 @@ pub async fn send_credential_request(state: AppState, action: Action) -> Result<
     Ok(state)
 }
 
+fn validate_credential_configuration(
+    credential_jwt: &serde_json::Value,
+    credential_configuration: &CredentialConfigurationsSupportedObject,
+) -> Result<(), AppError> {
+    let credential = get_unverified_jwt_claims(credential_jwt)?.get("vc").ok_or(AppError::InvalidCredentialFormatError)?;
+    
+    // validate credential against credential configuration
+    let credential_def = credential_configuration.credential_format.to_json();
+    Ok(())
+}
+
 /// Helper function to get the display name of a credential.
 fn get_credential_display_name(
     credential_configurations_supported: &HashMap<String, CredentialConfigurationsSupportedObject>,
@@ -326,6 +341,9 @@ fn get_credential_display_name(
 
 #[cfg(test)]
 mod tests {
+    use identity_iota::core::FromJson;
+    use oid4vc::oid4vci::credential_format_profiles::{CredentialFormats, WithParameters};
+
     use super::*;
 
     #[test]
@@ -433,5 +451,63 @@ mod tests {
 
         // Assert that the display name is equal to the last element of the `type` property of the credential.
         assert_eq!(display_name, "Credential Type 2");
+    }
+
+    #[test]
+    fn test_cred_config() {
+        let cred_jwt = "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImRpZDpqd2s6ZXlKaGJHY2lPaUpGVXpJMU5pSXNJbU55ZGlJNklsQXRNalUySWl3aWEybGtJam9pWjJsNVNqSmtaSFZIUmpGd2VsSkVUV3MzV1hKV2NGbG9MV3hWY0dOVVlrcDBYemgxT1RFeVdtSnZTU0lzSW10MGVTSTZJa1ZESWl3aWVDSTZJbUZJVHpSM0xWOTRWMHhuVVRSTlJGWlhVbXBNTWs1cmQzSnNOMGN4VlhwNWJuTnNSak14UjE5SFptc2lMQ0o1SWpvaVRXdzJNbTFsWDBOT1FsWkhXakZtYTBsUE5YWk9Xa1JGVVhoZldHTmpOMWRQVkZWbldrWlhTekZWWnlKOSMwIn0.eyJpc3MiOiJkaWQ6andrOmV5SmhiR2NpT2lKRlV6STFOaUlzSW1OeWRpSTZJbEF0TWpVMklpd2lhMmxrSWpvaVoybDVTakprWkhWSFJqRndlbEpFVFdzM1dYSldjRmxvTFd4VmNHTlVZa3AwWHpoMU9URXlXbUp2U1NJc0ltdDBlU0k2SWtWRElpd2llQ0k2SW1GSVR6UjNMVjk0VjB4blVUUk5SRlpYVW1wTU1rNXJkM0pzTjBjeFZYcDVibk5zUmpNeFIxOUhabXNpTENKNUlqb2lUV3cyTW0xbFgwTk9RbFpIV2pGbWEwbFBOWFpPV2tSRlVYaGZXR05qTjFkUFZGVm5Xa1pYU3pGVlp5SjkiLCJzdWIiOiJkaWQ6andrOmV5SmhiR2NpT2lKRlV6STFOaUlzSW1OeWRpSTZJbEF0TWpVMklpd2lhMmxrSWpvaU4xRnFhekpTZG1SbGNqTmxVMkZoYW1oeVNHRlZkMjgzU21oalNXNXhlbTVTU1dsVldVSmxiRFJoVFNJc0ltdDBlU0k2SWtWRElpd2llQ0k2SWxCVVpFbENWRUUxTjNrdFMwNHpXVXBXVjNWM2JXSlBja0Z3ZFhoZmFXTjFNekpPV0RKSlRuUnlPR01pTENKNUlqb2lka2cwZG1sck9GUTRkMGx3Y0hSTk1IZFFUamRNYVV0V2RFZHZVME5GV0VaMGNIVmFTMGd3WW0xamJ5SjkiLCJleHAiOjE3NzIwMTk4NjYsIm5iZiI6MTc0MDQ4Mzg2NiwiaWF0IjoxNzQwNDgzODY2LCJqdGkiOiJodHRwczovL3d3dy5kZWZlbnNpZS5ubC9vbmRlcndlcnBlbi9kaWVuamFhciIsInZjIjp7IkBjb250ZXh0IjpbImh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIiwiaHR0cHM6Ly9wdXJsLmltc2dsb2JhbC5vcmcvc3BlYy9vYi92M3AwL2NvbnRleHQtMy4wLjIuanNvbiJdLCJpZCI6Imh0dHBzOi8vd3d3LmRlZmVuc2llLm5sL29uZGVyd2VycGVuL2RpZW5qYWFyIiwidHlwZSI6WyJWZXJpZmlhYmxlQ3JlZGVudGlhbCIsIk9wZW5CYWRnZUNyZWRlbnRpYWwiXSwibmFtZSI6IkRpZW5qYWFyIERlZmVuc2llIiwiY3JlZGVudGlhbFN1YmplY3QiOnsiaWQiOiJkaWQ6andrOmV5SmhiR2NpT2lKRlV6STFOaUlzSW1OeWRpSTZJbEF0TWpVMklpd2lhMmxrSWpvaU4xRnFhekpTZG1SbGNqTmxVMkZoYW1oeVNHRlZkMjgzU21oalNXNXhlbTVTU1dsVldVSmxiRFJoVFNJc0ltdDBlU0k2SWtWRElpd2llQ0k2SWxCVVpFbENWRUUxTjNrdFMwNHpXVXBXVjNWM2JXSlBja0Z3ZFhoZmFXTjFNekpPV0RKSlRuUnlPR01pTENKNUlqb2lka2cwZG1sck9GUTRkMGx3Y0hSTk1IZFFUamRNYVV0V2RFZHZVME5GV0VaMGNIVmFTMGd3WW0xamJ5SjkiLCJ0eXBlIjpbIkFjaGlldmVtZW50U3ViamVjdCJdLCJhY2hpZXZlbWVudCI6eyJpZCI6Imh0dHBzOi8vd3d3LmRlZmVuc2llLm5sL29uZGVyd2VycGVuL2RpZW5qYWFyIiwidHlwZSI6IkFjaGlldmVtZW50IiwiYWxpZ25tZW50IjpbeyJ0eXBlIjoiQWxpZ25tZW50IiwidGFyZ2V0RGVzY3JpcHRpb24iOiJBbHRlciBvbmUncyBhdHRpdHVkZSBvciBiZWhhdmlvdXIgdG8gYWNjb21tb2RhdGUgbW9kaWZpY2F0aW9ucyBpbiB0aGUgd29ya3BsYWNlLiIsInRhcmdldE5hbWUiOiJBZGFwdCB0byBjaGFuZ2UiLCJ0YXJnZXRVcmwiOiJodHRwOi8vZGF0YS5ldXJvcGEuZXUvZXNjby9za2lsbC80OWRlOTk1OC0yYWE0LTRlZWYtYTg5ZC1mZTVkNWJjZDI4YzQifSx7InR5cGUiOiJBbGlnbm1lbnQiLCJ0YXJnZXREZXNjcmlwdGlvbiI6IlNraWxscyBhbmQgY29tcGV0ZW5jZXMgcmVxdWlyaW5nIGluZGl2aWR1YWxzIHRvIHVuZGVyc3RhbmQgYW5kIGNvbnRyb2wgdGhlaXIgb3duIGNhcGFiaWxpdGllcyBhbmQgbGltaXRhdGlvbnMgYW5kIHVzZSB0aGlzIHNlbGYtYXdhcmVuZXNzIHRvIG1hbmFnZSBhY3Rpdml0aWVzIGluIGEgdmFyaWV0eSBvZiBjb250ZXh0cy4gVGhleSBpbmNsdWRlIHRoZSBhYmlsaXR5IHRvIGFjdCByZWZsZWN0aXZlbHkgYW5kIHJlc3BvbnNpYmx5LCB0byBhY2NlcHQgZmVlZGJhY2ssIGFkYXB0aW5nIHRvIGNoYW5nZSBhbmQgdG8gc2VlayBvcHBvcnR1bml0aWVzIGZvciBwZXJzb25hbCBhbmQgcHJvZmVzc2lvbmFsIGRldmVsb3BtZW50LiIsInRhcmdldE5hbWUiOiJTZWxmLURpc2NpcGxpbmUiLCJ0YXJnZXRVcmwiOiJodHRwOi8vZGF0YS5ldXJvcGEuZXUvZXNjby9za2lsbC8wMjFhMjNlMS05MDdlLTQ2MjctYjA1YS01NTVmODg5Y2JiNjUifSx7InR5cGUiOiJBbGlnbm1lbnQiLCJ0YXJnZXREZXNjcmlwdGlvbiI6IkZvbGxvdyBpbnN0cnVjdGlvbnMgdG8gYWNoaWV2ZSBnb2FscyBhbmQgbWVldCBkZWFkbGluZXMuIiwidGFyZ2V0TmFtZSI6IkZvbGxvdyBJbnN0cnVjdGlvbnMiLCJ0YXJnZXRVcmwiOiJodHRwOi8vZGF0YS5ldXJvcGEuZXUvZXNjby9za2lsbC9hYjlmNTUwYy1mYThmLTRmZTgtOWY5Zi1lMDJkNDViZmY1NzAifSx7InR5cGUiOiJBbGlnbm1lbnQiLCJ0YXJnZXREZXNjcmlwdGlvbiI6IlRoZSBvcGVyYXRpb25zIG9mIHRoZSBzdXBwbHkgYW5kIGRlbWFuZCBvZiBnb29kcyBhbmQgcmVjb3VyY2VzIG9uIG1pbGl0YXJ5IGJhc2VzIGFuZCBkdXJpbmcgbWlsaXRhcnkgb3BlcmF0aW9ucyBvbiB0aGUgZmllbGQsIHRoZSBkaXNydXB0aW9uIG9mIGVuZW15IHN1cHBsaWVzLCBjb3N0IGFuYWx5c2lzLCBlcXVpcG1lbnQgZGVtYW5kcywgYW5kIG90aGVyIG1pbGl0YXJ5IGxvZ2lzdGljcyBhY3Rpdml0aWVzLiIsInRhcmdldE5hbWUiOiJNaWxpdGFyeSBMb2dpc3RpY3MiLCJ0YXJnZXRVcmwiOiJodHRwOi8vZGF0YS5ldXJvcGEuZXUvZXNjby9za2lsbC8yY2E0Yzg1Ny0xNDUwLTQwMDQtOWZkYi04NTAwZGVmNmI4ZmEifSx7InR5cGUiOiJBbGlnbm1lbnQiLCJ0YXJnZXREZXNjcmlwdGlvbiI6IlBsYW4gdGhlIHRpbWUgc2VxdWVuY2Ugb2YgZXZlbnRzLCBwcm9ncmFtbWVzIGFuZCBhY3Rpdml0aWVzLCBhcyB3ZWxsIGFzIHRoZSB3b3JrIG9mIG90aGVycy4iLCJ0YXJnZXROYW1lIjoiVGltZSBNYW5hZ2VtZW50IiwidGFyZ2V0VXJsIjoiaHR0cDovL2RhdGEuZXVyb3BhLmV1L2VzY28vc2tpbGwvZDkwMTNlMGUtZTkzNy00M2Q1LWFiNzEtMGU5MTdlZTg4MmI4In1dLCJhY2hpZXZlbWVudFR5cGUiOiJDb21wZXRlbmN5IiwiY3JpdGVyaWEiOnsibmFycmF0aXZlIjoiRm9sbG93IGEgZnVsbCB5ZWFyIG9mIHNlcnZpY2UgYXQgdGhlIER1dGNoIE1pbGl0YXJ5LiJ9LCJkZXNjcmlwdGlvbiI6Ik9uZSB5ZWFyLCB5b3Ugd29yayBhcyBzb2xkaWVyIGF0IHRoZSBNYXJpbmVzLCBBcm15LCBBaXJmb3JjZSwgb3IgTWFyZWNoYXVzc2VlLiAgWW91IGRpc2NvdmVyIHRoZSBpbm5lci13b3JraW5ncyBEdXRjaCBNaWxpdGFyeSwgbGVhcm4gZXZlcnl0aGluZyBhYm91dCB0ZWFtd29yaywgZGVhbGluZyB3aXRoIHNldGJhY2tzLCByZXNwb25zaWJpbGl0eSBhbmQgdHJ1c3RpbmcgeW91ciBvd24gc3RyZW5ndGguIFlvdSB3aWxsIGZvbGxvdyBhIHRyYWluaW5nIGluIG1pbGl0YXJ5IHNraWxscyBhbmQgbGVhcm4gdG8ga25vdyBhbmQgb3BlcmF0ZSBpbXByZXNzaXZlIHdlYXBvbiBzeXN0ZW1zLiBZb3Ugd2lsbCBiZSBjaGFsbGVuZ2VkIGJvdGggcGh5c2ljYWxseSBhbmQgbWVudGFsbHkgdG8gc3RlcCBvdXQgb2YgeW91ciBjb21mb3J0IHpvbmUuIiwibmFtZSI6IkRpZW5qYWFyIn19LCJpc3N1ZXIiOiJkaWQ6andrOmV5SmhiR2NpT2lKRlV6STFOaUlzSW1OeWRpSTZJbEF0TWpVMklpd2lhMmxrSWpvaVoybDVTakprWkhWSFJqRndlbEpFVFdzM1dYSldjRmxvTFd4VmNHTlVZa3AwWHpoMU9URXlXbUp2U1NJc0ltdDBlU0k2SWtWRElpd2llQ0k2SW1GSVR6UjNMVjk0VjB4blVUUk5SRlpYVW1wTU1rNXJkM0pzTjBjeFZYcDVibk5zUmpNeFIxOUhabXNpTENKNUlqb2lUV3cyTW0xbFgwTk9RbFpIV2pGbWEwbFBOWFpPV2tSRlVYaGZXR05qTjFkUFZGVm5Xa1pYU3pGVlp5SjkiLCJpc3N1YW5jZURhdGUiOiIyMDI1LTAyLTI1VDExOjQ0OjI2WiJ9fQ.zbCo6pJxiJ48ArcKug5_4zVQ1yvbzq-G2lakl5G2myL_6Y5jx-Bt9toHLWXA0RqCKnRaBGrOoIMVcNMizuA8dg";
+        let cred_conf: CredentialFormats<WithParameters> = CredentialFormats::from_json(r#"
+            {
+                "format": "
+                "credential_format": { 
+                    "parameters": { 
+                        "credential_definition": { 
+                            "type_": ["VerifiableCredential", "OpenBadgeCredential"], 
+                            "credential_subject": { 
+                                "credential_subject": {
+                                    "achievement": {
+                                        "achievementType": {
+                                            "value_type": "string", 
+                                            "display": [{
+                                                "name": "Achievement Type", 
+                                                "locale": "en-US"
+                                            }]
+                                        }, 
+                                        "criteria": {
+                                            "narrative": {
+                                                "value_type": "string", 
+                                                "display":  [{
+                                                    "locale": "en-US", 
+                                                    "name": "Criteria"
+                                                }]
+                                            }
+                                        },
+                                        "alignment": {
+                                            "value_type": "array", 
+                                            "targetDescription": {
+                                                "display": [{
+                                                    "locale": "en-US", 
+                                                    "name": "Description"
+                                                }], 
+                                                "value_type": "string"
+                                            },
+                                            "targetName": {
+                                                "display": [{
+                                                    "locale": "en-US", 
+                                                    "name": "Name"
+                                                }], 
+                                                "value_type": "string"
+                                            }
+                                        }
+                                    }
+                                } 
+                            } 
+                        }, 
+                        "order": null 
+                    }
+                }
+        }
+        "#
+        ).unwrap();
     }
 }
