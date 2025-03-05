66e5f96153ac847912916f836e4bf21ba1ff8a1d
Daniel Mader <daniel.mader@impierce.com>
2025-03-05 10:22:13 +0100

    feat: update description, skip info page conditionally

--- unime/src/routes/(app)/me/add/email/info/+page.svelte
+++ unime/src/routes/(app)/me/add/email/info/+page.svelte
@@ -1,15 +1,40 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   import { goto } from '$app/navigation';
   import LL from '$i18n/i18n-svelte';
 
   import { Accordion, Button, IconMessage, TopNavBar } from '$lib/components';
   import { EnvelopeOpenFillIcon } from '$lib/icons';
+  import { state } from '$lib/stores';
 
   const items = [
-    { id: '0', title: 'What can I do with my verified email?', description: 'Present ownership over that email.' },
-    { id: '1', title: 'How is it verified?', description: 'Code inside email.' },
-    { id: '2', title: 'Is my personal information safe?', description: 'Yes.' },
+    {
+      id: '0',
+      title: 'What can I do with my verified email?',
+      description:
+        'You can prove ownership of that email address to websites, shops and other people. It also allows you to log in to websites that support it without needing to remember a password or setting up a passkey.',
+    },
+    {
+      id: '1',
+      title: 'How is it verified?',
+      description:
+        'UniMe sends a message containing your email to one of our verification systems. That system then creates a one-time code and sends an email to your inbox containing that code. When you enter that code in UniMe, you prove you have access to that email inbox. UniMe now sends another message to our verification system, redeeming the code for a verifiable credential.',
+    },
+    {
+      id: '2',
+      title: 'Is my personal information safe?',
+      description:
+        'We treat any data you enter in UniMe with the greatest respect. All messages from UniMe to our servers are fully encrypted when they travel through the internet. They are also encrypted on our servers using the latest security standards. In order to successfully deliver an email to your inbox, we initially have to receive it in plain text, so a full "zero-access encryption" is unfortunately not applicable to this use case.',
+    },
   ];
+
+  onMount(() => {
+    // If there is already an active verification session ongoing, skip this info page.
+    if ($state.verified_data.email_verification) {
+      goto('/me/add/email');
+    }
+  });
 </script>
 
 <TopNavBar on:back={() => history.back()} title={'Verified email'} class="sticky top-0 z-10" />
