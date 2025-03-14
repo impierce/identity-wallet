db1924ae1867cea3f4cfdbfafa5af2edd7c88005
Daniel Mader <daniel.mader@impierce.com>
2025-03-14 14:36:22 +0100

    feat: add `es_ES` translations

---
+++ unime/src/i18n/es-ES/index.ts
@@ -0,0 +1,430 @@
+import type { BaseTranslation } from '../i18n-types';
+
+const es_ES = {
+  ONBOARDING: {
+    WELCOME: {
+      GREETING: 'Bienvenidos a',
+      WHAT_IS_UNIME_1: 'UniMe conecta tu mundo digital, de forma segura y protegida.',
+      WHAT_IS_UNIME_2: 'Para empezar, crea un perfil de identidad completamente nuevo.',
+      CREATE_NEW_PROFILE: 'Crea un perfil nuevo',
+      SELECT_LANGUAGE: 'Selecciona el idioma',
+    },
+    PLEDGE: {
+      NAVBAR_TITLE: 'El compromiso UniMe',
+      TITLE_1: 'Nada de',
+      TITLE_2: 'cosas raras',
+      SUBTITLE: 'Este es nuestro compromiso contigo.',
+      ITEM_1: {
+        TITLE: 'No compartiremos tus datos',
+        DESCRIPTION:
+          'Tus datos son tuyos y solo tú decides con quién los compartes. Punto. De hecho, tus datos ni siquiera entran en nuestros sistemas, a menos que selecciones una de las opciones de almacenaje en la nube.',
+      },
+      ITEM_2: {
+        TITLE: 'No añadiremos rastreadores',
+        DESCRIPTION:
+          'No rastreamos tu actividad en segundo plano. Punto. Ni para hacer pruebas ni por ningún otro motivo. Este es nuestro compromiso. Tampoco capturamos ninguna información anónima del dispositivo ni estadísticas de uso. Esta decisión hace que desarrollar la aplicación nos resulte algo más difícil, pero creemos que es la decisión correcta.',
+      },
+      ITEM_3: {
+        TITLE: 'Tu información es tuya',
+        DESCRIPTION: 'Creemos que ya es hora de que vuelvas a ser el dueño de tu información personal.',
+      },
+    },
+    TERMS: {
+      NAVBAR_TITLE: 'Términos y condiciones',
+      TITLE_1: 'Aquí está lo menos interesante',
+      TITLE_2: 'cosas',
+      SUBTITLE: 'Sí, ya lo sabemos. Sin embargo, recomendamos que leas esta información con atención.',
+      T_AND_C: {
+        TITLE: 'Términos y condiciones',
+        DESCRIPTION: 'He leído y estoy de acuerdo con los términos y condiciones.',
+        DIALOG_TITLE: 'Términos y condiciones para Impierce Technologies B.V',
+        LAST_UPDATED: 'Última actualización: 31 de octubre de 2024',
+        TL_DR:
+          'Como con cualquier aplicación, nosotros, Impierce Technologies, estamos obligados a informarte sobre una serie de términos legales. Intentamos mantenerlo simple y directo, pero estamos limitados por lo que debe decirse para protegernos. La versión corta es que nos enfocamos en proporcionar una aplicación que respeta tu privacidad, no recopila datos y te da control total sobre tu información. Como eres tú quien decide qué datos recopilas y con quién los compartes, eres completamente responsable de estas decisiones. Además, dado que no recopilamos tu información, cualquier pérdida de datos debido a la pérdida de tu dispositivo o del acceso a la aplicación también es tu responsabilidad. Con un gran poder viene una gran responsabilidad. Ahora, la versión aburrida:',
+        FULL: `Bienvenido a Impierce Technologies B.V. (“nosotros”, “nos” o “nuestro”). Estos Términos y Condiciones (“Términos”) regulan tu acceso a la aplicación UniMe - Identity Wallet (“Servicio”). Al acceder o usar nuestro Servicio, aceptas cumplir con estos Términos. Si no estás de acuerdo con alguna parte de estos Términos, por favor no utilices nuestro Servicio.
+
+Aceptación de los Términos
+Al descargar, acceder o utilizar nuestro Servicio, confirmas que has leído, comprendido y aceptas estos Términos y nuestra Política de Privacidad. Si utilizas el Servicio en nombre de una organización, declaras que tienes la autoridad para vincular a dicha organización a estos Términos.
+
+
+Responsabilidades del usuario
+Seguridad de los datos: Usted es responsable de mantener la confidencialidad y seguridad de sus dispositivos y cualquier credencial utilizada para acceder a nuestro Servicio. Impierce Technologies B.V. no se hace responsable de ninguna pérdida o compromiso de sus datos debido a que usted no haya protegido su dispositivo.
+Cumplimiento de la legislación: Usted se compromete a utilizar nuestro Servicio de conformidad con todas las leyes y reglamentos aplicables.
+Información precisa: Usted es responsable de garantizar que cualquier información que almacene o comparta utilizando nuestro Servicio sea precisa y esté actualizada.
+
+
+
+Propiedad y privacidad de los datos
+No se recogen datos: No recopilamos, almacenamos ni procesamos ninguna información personal a través de la aplicación UniMe. Todos los datos de identidad permanecen únicamente en tu dispositivo o en la opción de almacenamiento que elijas.
+Uso compartido controlado por el usuario: Cualquier intercambio de datos es iniciado y gestionado exclusivamente por usted y se produce directamente entre usted y la parte designada. Nosotros no accedemos, rastreamos ni tenemos conocimiento de su identidad o actividad.
+Recogida de datos por terceros: Tenga en cuenta que las plataformas de terceros como Apple App Store o Google Play Store pueden recopilar datos relacionados con el rendimiento de la aplicación, el uso y la información del dispositivo independientemente de Impierce Technologies B.V.
+
+
+
+
+Derechos de propiedad intelectual
+Propiedad y licencias: Todos los derechos de propiedad intelectual de UniMe y los materiales relacionados son propiedad de Impierce Technologies B.V. o de sus licenciantes. No obstante, UniMe se proporciona como producto de código abierto bajo licencia Apache 2.0, lo que permite a los usuarios utilizar, modificar y distribuir el software de acuerdo con los términos de dicha licencia.
+Concesión de licencia: Al descargar o utilizar UniMe, acepta cumplir los términos de la licencia Apache 2.0, que concede una licencia mundial libre de derechos de autor para utilizar, reproducir y distribuir el software con la atribución adecuada.
+
+Para más detalles, consulta el texto completo de la Licencia Apache 2.0.
+
+
+
+Actividades prohibidas
+Usted se compromete a no realizar ninguna de las siguientes actividades:
+Acceso no autorizado: Intentar obtener acceso no autorizado a nuestro Servicio o sistemas.
+Interferencia: Interrumpir o interferir en la seguridad o el funcionamiento del Servicio.
+Uso indebido: Utilizar el Servicio con fines ilícitos, perjudiciales, fraudulentos, infractores u otros fines malintencionados.
+
+
+
+Servicios de terceros
+Nuestro Servicio puede incluir enlaces o integraciones con Servicios de terceros. No controlamos estos Servicios de terceros y no somos responsables de su contenido o políticas. El uso que usted haga de los Servicios de terceros corre por su cuenta y riesgo y está sujeto a sus términos y condiciones.
+
+
+
+Descargo de responsabilidad
+Tal cual: Nuestro Servicio se proporciona tal cual y según disponibilidad, sin garantías de ningún tipo, ni expresas ni implícitas.
+Sin garantía: No garantizamos que el Servicio sea ininterrumpido, esté libre de errores, sea seguro o esté libre de virus u otros componentes dañinos.
+
+
+
+Limitación de responsabilidad
+En la máxima medida permitida por la ley, Impierce Technologies B.V. no será responsable de ningún daño indirecto, incidental, especial, consecuente o punitivo, incluyendo pero no limitado a la pérdida de beneficios, datos, uso, fondo de comercio u otras pérdidas intangibles resultantes de:
+Su acceso o uso o incapacidad de acceder o utilizar el Servicio;
+Cualquier conducta o contenido de terceros en el Servicio;
+Cualquier contenido obtenido del Servicio.
+
+
+
+Indemnización
+Usted se compromete a indemnizar, defender y mantener indemne a Impierce Technologies B.V., sus filiales, y sus respectivos directores, funcionarios, empleados y agentes de y contra cualquier reclamación, responsabilidad, daños, pérdidas y gastos derivados de o relacionados de alguna manera con:
+Su acceso o uso del Servicio;
+Su violación de estas Condiciones;
+Su violación de los derechos de terceros.
+
+
+
+Modificaciones de las Condiciones
+Podemos actualizar o modificar estas Condiciones de vez en cuando. La fecha de «Última actualización» en la parte superior indica cuándo se realizaron los últimos cambios. Le notificaremos los cambios significativos publicando las nuevas Condiciones en nuestro sitio web o a través del Servicio. Su uso continuado del Servicio después de cualquier cambio constituye su aceptación de las nuevas Condiciones.
+
+
+
+Ley aplicable y jurisdicción
+Las presentes Condiciones se regirán e interpretarán de conformidad con la legislación de los Países Bajos. Cualquier disputa que surja en relación con estas Condiciones estará sujeta a la jurisdicción exclusiva de los tribunales de los Países Bajos.
+
+
+
+Divisibilidad
+Si alguna disposición de estas Condiciones se considera inválida o inaplicable, las disposiciones restantes permanecerán en pleno vigor y efecto.
+
+
+
+Acuerdo completo
+Estas Condiciones, junto con nuestra Política de privacidad, constituyen el acuerdo completo entre usted e Impierce Technologies B.V. en relación con el uso del Servicio.
+
+
+
+Información de contacto
+Para cualquier pregunta, duda o comentario sobre estas Condiciones, póngase en contacto con nosotros en:
+Impierce Technologies B.V.
+Karekietweide 6, 3993 CD, Houten, the Netherlands
+Email: contact@impierce.com
+
+
+
+Idioma
+Estas Condiciones pueden haber sido traducidas a otros idiomas para su comodidad. En caso de conflicto entre la versión inglesa y una versión traducida, prevalecerá la versión inglesa.
+
+Al utilizar nuestro Servicio, usted reconoce que ha leído, entendido y aceptado estos Términos y Condiciones. Impierce Technologies B.V. mantiene su compromiso de proporcionarle soluciones seguras y centradas en la privacidad, respetando al mismo tiempo sus derechos y libertades.
+`,
+      },
+      OWNERSHIP: {
+        TITLE: 'Propiedad de los datos',
+        DESCRIPTION: 'Entiendo que soy el único responsable de mis datos.',
+      },
+    },
+    CUSTOMIZE: {
+      NAVBAR_TITLE: 'Personalización',
+      NAME: {
+        TITLE_1: '¡Adelante! Escoge un',
+        TITLE_2: 'nombre de perfil',
+        SUBTITLE: 'La información de tu perfil nunca saldrá de tu dispositivo.',
+        INPUT_PLACEHOLDER: 'Introduce un nombre de perfil',
+      },
+      PICTURE: {
+        TITLE_1: 'Establece una imagen',
+        TITLE_2: 'foto',
+        SUBTITLE: 'Personalízala.',
+      },
+    },
+    PASSWORD: {
+      NAVBAR_TITLE: 'Contraseña',
+      TITLE_1: 'Establece tu nueva',
+      TITLE_2: 'contraseña',
+      SUBTITLE: 'Debes escoger una contraseña fuerte para encriptar tus datos de manera segura.',
+      INPUT_PLACEHOLDER: 'Introduce una contraseña',
+      CONFIRM: {
+        NAVBAR_TITLE: 'Confirma la contraseña',
+        TITLE_1: 'Confirma tu nueva',
+        TITLE_2: 'contraseña',
+        SUBTITLE: 'Tienes que confirmar tu contraseña para asegurarte de que la has escrito correctamente.',
+        INPUT_PLACEHOLDER: 'Vuelve a escribir tu contraseña',
+        MATCH: 'Las contraseñas coinciden',
+        NO_MATCH: 'Las contraseñas no coinciden',
+      },
+      COMPLETED: {
+        NAVBAR_TITLE: 'Contraseña establecida',
+        TITLE_1: 'Tu perfil UniMe ya está',
+        TITLE_2: 'protegido',
+        MESSAGE_1: 'Seguro y protegido.',
+        MESSAGE_2: 'Buen trabajob',
+      },
+    },
+  },
+  SETTINGS: {
+    NAVBAR_TITLE: 'Ajustes',
+    PROFILE: {
+      TITLE: 'Mi perfil',
+      PROFILE_NAME: {
+        TITLE: 'Nombre del perfil',
+        NAVBAR_TITLE: 'Cambia el nombre del perfil',
+        INPUT_PLACEHOLDER: 'Ingresa un nombre de perfil',
+        CONFIRM: 'Actualiza',
+      },
+      DISPLAY_PICTURE: {
+        EDIT: 'Edita',
+        CHANGE: 'Selecciona una foto del perfil',
+        REMOVE: 'Borra',
+      },
+      DELETE_PROFILE: {
+        TITLE: 'Borra el perfil',
+      },
+    },
+    APP: {
+      TITLE: 'Configuración de la aplicación',
+      NAVBAR_TITLE: 'Configuración de la aplicación',
+      LANGUAGE: {
+        TITLE: 'Idioma',
+        NAVBAR_TITLE: 'Seleccionar idioma',
+        COMING_SOON: 'Próximamente',
+      },
+      THEME: {
+        LABEL: 'Tema',
+        NAVBAR_TITLE: 'Seleccionar tema',
+        TITLE_1: 'Escoge el aspecto de tu',
+        TITLE_2: 'aplicación',
+        SUBTITLE: '¿Te gusta más la noche?',
+      },
+      PASSWORD: {
+        TITLE: 'Contraseña',
+      },
+      ONBOARDING_JOURNEY: {
+        TITLE: 'Proceso de incorporación',
+        BUTTON_TEXT: 'Reinicia',
+      },
+      HINTS_AND_TIPS: {
+        TITLE: 'Sugerencias y consejos',
+        BUTTON_TEXT: 'Restablece',
+      },
+      DEVELOPER_MODE: {
+        TITLE: 'Modo desarrollador',
+      },
+    },
+    BACKUP_RECOVERY: {
+      TITLE: 'Copia de seguridad y recuperación',
+    },
+    LOG_OUT: {
+      TITLE: 'Cierra la sesión',
+    },
+    THEME: {
+      SYSTEM: 'Sistema',
+      LIGHT: 'Luz',
+      DARK: 'Oscuro',
+    },
+    PASSWORD: {
+      POLICY: {
+        TITLE: 'Tu contraseña debe contener',
+        UPPERCASE_LETTER: 'letra mayúscula',
+        LOWERCASE_LETTER: 'letra minúscula',
+        NUMBER: 'número',
+        CHARACTERS: 'caracteres',
+      },
+    },
+    RESET_APP: {
+      TITLE: 'Restablece la aplicación',
+      DESCRIPTION: '¿Seguro que quieres restablecer la aplicación y eliminar todos los datos?',
+      CONFIRM: 'Sí, elimina todo',
+      CANCEL: 'No, conserva mi perfil',
+    },
+    ACCOUNT: 'Cuenta',
+    SUPPORT: {
+      TITLE: 'Soporte',
+      ABOUT: {
+        TITLE: 'Sobre UniMe',
+        NAVBAR_TITLE: 'Sobre UniMe',
+        SPECIFICATIONS: 'Especificaciones',
+        VERSION: 'Versión',
+        LICENSE: 'Licencia',
+        BUILT_WITH: 'Construida con Tauri',
+      },
+      FEEDBACK: {
+        TITLE: 'Envía feedback',
+      },
+    },
+  },
+  LOCK_SCREEN: {
+    PASSWORD_INPUT_PLACEHOLDER: 'Introduce tu contraseña',
+    BUTTON_TEXT: 'Desbloquea la billetera',
+    FORGOT_PASSWORD: '¿Has olvidado tu contraseña?',
+  },
+  ME: {
+    BOTTOM_NAVIGATION_TITLE: 'Yo',
+    GREETINGS: {
+      GREETING_0: 'Hola',
+      GREETING_1: 'Qué tal',
+      GREETING_2: 'Cómo estás',
+      GREETING_3: 'Bienvenido de nuevo',
+      GREETING_4: 'Hola',
+    },
+    DEMO: 'Visita una de las siguientes páginas web en un ordenador de sobremesa para comenzar.',
+    CREDENTIAL_TABS: {
+      ALL: 'Todo',
+      DATA: 'Datos',
+      BADGES: 'Insignias',
+    },
+    EMPTY_CREDENTIALS: {
+      TITLE: 'Esto está un poco tranquilo',
+      SUBTITLE: '¿Qué tal si añades algunas credenciales para empezar tu nuevo yo digital?',
+    },
+    FAVORITES: 'Mis favoritos',
+  },
+  ACTIVITY: {
+    BOTTOM_NAVIGATION_TITLE: 'Actividad',
+    NAVBAR_TITLE: 'Conectado',
+    TABS: {
+      CONNECTIONS: 'Conexiones',
+      HISTORY: 'Historial',
+    },
+  },
+  SCAN: {
+    BOTTOM_NAVIGATION_TITLE: 'Escanea',
+    TITLE_1: 'Escanea un',
+    TITLE_2: 'código QR',
+    SUBTITLE: 'Coloca un código QR en la pantalla para iniciar una interacción.',
+    PERMISSION_DENIED: 'No tenemos permiso para acceder a la cámara',
+    OPEN_SETTINGS: 'Abre la pantalla de ajustes',
+    CREDENTIAL_OFFER: {
+      NAVBAR_TITLE: 'Oferta de credenciales',
+      DESCRIPTION: 'te ofrece las siguientes credenciales',
+      ACCEPT: 'Acepta las credenciales',
+    },
+    CONNECTION_REQUEST: {
+      NAVBAR_TITLE: 'Solicitud de conexión',
+      TITLE: 'Nueva conexión',
+      DESCRIPTION: 'Acepta únicamente las nuevas conexiones que reconozcas y en las que confíes',
+      CONNECTED_PREVIOUSLY: 'Conectado previamente',
+      ACCEPT: 'Acepta la conexión',
+    },
+    SHARE_CREDENTIALS: {
+      NAVBAR_TITLE: 'Comparte datos',
+      DESCRIPTION: 'solicita las siguientes credenciales',
+      REQUESTED: 'Solicitadas',
+      APPROVE: 'Aprueba la petición',
+    },
+  },
+  CONNECTION: {
+    TABS: {
+      SUMMARY: 'Resumen',
+      DATA: 'Datos',
+      ACTIVITY: 'Actividad',
+    },
+    SUMMARY: {
+      EMPTY: 'Todavía sin conexiones.',
+      TITLE: 'Conectado a',
+      FIRST_CONNECTED: 'Primer conectado',
+      LAST_CONNECTED: 'Último conectado',
+    },
+    DATA: {
+      EMPTY: 'Todavía sin datos.',
+    },
+  },
+  HISTORY: {
+    EMPTY: 'Todavía sin actividad.',
+    DATA_RECEIVED: 'Datos recibidos de',
+    DATA_SHARED: 'Datos compartidos con',
+    CONNECTION_ADDED: 'Conectado a',
+  },
+  SEARCH: {
+    INPUT_PLACEHOLDER: 'Busca algo',
+    NO_QUERY: {
+      TITLE: '¿Qué quieres que busquemos?',
+      DESCRIPTION: 'Busca aquí alguna de tus credenciales e insignias.',
+    },
+    NO_RESULTS: {
+      TITLE: 'No se ha encontrado ningún resultado',
+      DESCRIPTION: 'Intenta buscar otra cosa.',
+    },
+    RECENT_SEARCHES: 'Búsquedas recientes',
+  },
+  CREDENTIAL: {
+    NAVBAR_TITLE: 'Información de las credenciales',
+    DETAILS: {
+      VALID: 'Válidas',
+      ISSUED_BY: 'Emitidas por',
+      DESCRIPTION: 'Descripción',
+      OPEN_BADGES: {
+        ALIGNMENT: 'Alineación',
+        CRITERIA: 'Criterio',
+      },
+    },
+    ACTIONS: {
+      DELETE: {
+        BUTTON_LABEL: 'Borra las credenciales',
+        TITLE: 'Borra las credenciales',
+        DESCRIPTION: '¿Seguro que quieres borrar estas credenciales de tu billetera? Esta acción no puede deshacerse.',
+        CONFIRM_BUTTON: 'Borra',
+      },
+    },
+  },
+  SORT: {
+    TITLE: 'Clasificación',
+    PREFERENCES: {
+      LIST_VIEW: 'Vista de lista',
+      GRID_VIEW: 'Vista de cuadrícula',
+      ALPHABETICAL: 'Alfabético',
+      DATE_ISSUED: 'Fecha de emisión',
+      DATE_ADDED: 'Fecha añadida',
+    },
+    ORDER: {
+      A_Z: 'A to Z',
+      Z_A: 'Z to A',
+      NEWEST: 'Primero la más nueva',
+      OLDEST: 'Primero la más antigua',
+    },
+  },
+  DOMAIN_LINKAGE: {
+    TITLE: 'Página web verificada',
+    SUCCESS: 'UniMe ha verificado correctamente la identidad para darte un inicio de sesión seguro.',
+    FAILURE: 'UniMe no pudo verificar la vinculación de la identidad al dominio.',
+    UNKNOWN: 'UniMe no puedo encontrar ninguna prueba de la identidad asociada al dominio.',
+    CAUTION: '¡Proceder con precaución!',
+  },
+  ERROR: {
+    TITLE: '¡Vaya!',
+    DEFAULT_MESSAGE: 'Algo salió mal. Vuelve a intentarlo.',
+  },
+  CANCEL: 'Cancela',
+  CLOSE: 'Cierra',
+  CONTINUE: 'Continua',
+  SKIP: 'Saltar',
+  ACCEPT: 'Acepta',
+  REJECT: 'Rechaza',
+  // TODO: AI generated to fix TS errors in journeys.
+  GETTING_STARTED: {
+    SKIP_TITLE: 'Salta el paso de empezar',
+    SKIP_TEXT: '¿Seguro que quieres saltarte la guía de cómo empezar?',
+  },
+} satisfies BaseTranslation;
+
+export default es_ES;
