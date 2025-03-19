cb21f54b9e101b1c0554f4c6bc371fb24f004b48
Daniel Mader <daniel.mader@impierce.com>
2025-03-19 22:49:07 +0100

    refactor: reorder dependencies

--- unime/src-tauri/src/lib.rs
+++ unime/src-tauri/src/lib.rs
@@ -40,9 +40,8 @@ pub fn run() {
                 .build(),
         )
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_os::init())
-        // .plugin(tauri_plugin_cloud_storage::init())
+        .plugin(tauri_plugin_shell::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
