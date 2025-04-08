9aa723388a38ae8d3656c4a6c921186780e75ffe
M-Adam-Hus <167335223+M-Adam-Hus@users.noreply.github.com>
2025-04-08 16:26:09 +0200

    Update index.ts

--- unime/src/i18n/en-GB/index.ts
+++ unime/src/i18n/en-GB/index.ts
@@ -4,11 +4,13 @@ import en from '../en';
 import type { Translation } from '../i18n-types';
 
 const en_GB = extendLanguage(en, {
-  ONBOARDING: {
+ONBOARDING: {
     CUSTOMIZE: {
       NAVBAR_TITLE: 'Customisation',
     },
     PLEDGE: {
+      TITLE_1: 'No dodgy',
+      TITLE_2: 'dealings',
       ITEM_1: {
         DESCRIPTION:
           'Your data belongs to you and only you decide who you share it with. Full stop. In fact, your data never even touches any of our systems - unless you opt-in to one of the cloud storage options.',
@@ -17,11 +19,19 @@ const en_GB = extendLanguage(en, {
         DESCRIPTION:
           "We do not track your actions behind the scenes. Full stop. Not for testing or any other reasons. That's our pledge. We also do not collect any anonymous device information or usage statistics. That decision makes developing the app a bit harder for us, but we believe it is the right decision.",
       },
-    },
+    }, 
+  },
+  SCAN:{
+    CONNECTION_REQUEST:{
+      DESCRIPTION: 'Only accept new connections that you recognise and trust'
+    }
   },
   ME: {
     FAVORITES: 'My favourites',
   },
+ERROR: {
+  TITLE: 'Oh dear!',
+}
 }) satisfies Translation;
 
 export default en_GB;
