3bf461e5f974998bc9a0f14adddf1418e2ec921d
Oran Dan <oran-dan@hotmail.com>
2025-05-16 18:19:38 +0200

    chore: remove comments

--- identity-wallet/src/state/did/validate_linked_verifiable_presentations.rs
+++ identity-wallet/src/state/did/validate_linked_verifiable_presentations.rs
@@ -445,7 +445,7 @@ fn extract_url_from_did_web(did_web: &str) -> Option<Url> {
     None
 }
 
-// #[cfg(not(feature = "test_utils"))]
+#[cfg(not(feature = "test_utils"))]
 #[cfg(test)]
 mod tests {
     use super::*;
