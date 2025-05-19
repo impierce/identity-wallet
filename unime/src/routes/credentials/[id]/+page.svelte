02849a1ae0dc8066e20b0af5eb53743fd81d0748
Daniel Mader <daniel.mader@impierce.com>
2025-05-19 21:37:48 +0200

    feat: edit credential display name

--- unime/src/routes/credentials/[id]/+page.svelte
+++ unime/src/routes/credentials/[id]/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import LL from '$i18n/i18n-svelte';
+  import { fly } from 'svelte/transition';
 
   import type { DisplayCredential } from '@bindings/credentials/DisplayCredential';
 
-  import { pageTitleStore, state } from '$lib/stores';
+  import { Button } from '$lib/components';
+  import { dispatch } from '$lib/dispatcher';
+  import { state as appState, pageTitleStore } from '$lib/stores';
 
   import CredentialHeader from './CredentialHeader.svelte';
   import CredentialOverview from './CredentialOverview.svelte';
@@ -14,15 +17,20 @@
   // Credential cannot be loaded via load function since it's stored in the application state.
   // TODO Credential should be loaded from backend via load function to handle invalid IDs properly.
   function loadCredential() {
-    return $state.credentials.find((c) => $page.params.id === c.id);
+    return $appState.credentials.find((c) => $page.params.id === c.id);
   }
 
   let credential: DisplayCredential | undefined = loadCredential();
 
+  let displayName: string = credential?.display_name ?? credential?.data.name;
+  let displayNameUpdated = displayName;
+
+  let openEditMode = false;
+
   // The credential's metadata, e.g., the favorite status, may change.
   // ESLint does not understand the reactive statement.
   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-  $: $state, (credential = loadCredential());
+  $: $appState, (credential = loadCredential());
 
   $: {
     // Use `pageTitleStore` to get page title into layout.
@@ -30,15 +38,54 @@
   }
 
   const credentialTypes = credential?.data?.type as string[] | undefined;
+
+  function discard() {
+    displayNameUpdated = displayName;
+    openEditMode = false;
+  }
+
+  async function update() {
+    await dispatch({ type: '[Credential Metadata] Update', payload: { id: credential?.id, name: displayNameUpdated } });
+    openEditMode = false;
+  }
 </script>
 
 {#if credential}
   <div class="flex min-h-full flex-col gap-7 bg-background-alt px-4 pb-7">
     <CredentialHeader {credential}>
-      <h1 class="text-center font-semibold">
-        {credential.data?.name ?? credential.display_name}
-      </h1>
+      <!-- Editable title -->
+      <input
+        type="text"
+        bind:value={displayNameUpdated}
+        class="-my-2 h-10 w-fit bg-background text-center font-semibold focus:outline-none"
+        placeholder="Some title"
+        onfocus={() => (openEditMode = true)}
+        onblur={() => {
+          // If no change was made, automatically close edit mode when input loses focus.
+          if (displayName === displayNameUpdated) {
+            openEditMode = false;
+          }
+        }}
+      />
     </CredentialHeader>
+    {#if openEditMode}
+      <!-- Similar to ActionSheet, but without backdrop -->
+      <div
+        class="fixed bottom-0 left-0 flex w-screen flex-col space-y-[10px] rounded-t-[20px] bg-background-alt p-6"
+        transition:fly={{
+          y: 350,
+          duration: 300,
+          opacity: 1,
+        }}
+      >
+        <Button label="Discard changes" variant="secondary" on:click={discard} />
+        <Button
+          label="Update display name"
+          disabled={displayNameUpdated.length === 0 || displayNameUpdated === displayName}
+          on:click={update}
+        />
+      </div>
+    {/if}
     <CredentialOverview {credential} />
     {#if credentialTypes?.includes('OpenBadgeCredential') || credentialTypes?.includes('AchievementCredential')}
       <OpenBadgeRenderer {credential} />
