994e3f1694ef97eaf280398c9ebb4660316fc422
Daniel Mader <daniel.mader@impierce.com>
2025-05-21 15:10:10 +0200

    feat: add `variant` and `dismissible` props

--- unime/src/lib/components/ErrorToast.svelte
+++ unime/src/lib/components/ErrorToast.svelte
@@ -3,16 +3,18 @@
 
   import { fade, fly } from 'svelte/transition';
 
-  import { WarningRegularIcon, XRegularIcon } from '$lib/icons';
+  import { InfoRegularIcon, WarningRegularIcon, XRegularIcon } from '$lib/icons';
 
   interface Props {
+    variant: 'error' | 'info';
+    dismissible?: boolean;
     autoDismissAfterMs?: number; // A value of 0 means the toast will never auto-dismiss
     title: string;
     detail?: string;
     ondismissed?: () => void;
   }
 
-  let { autoDismissAfterMs = 0, title, detail, ondismissed }: Props = $props();
+  let { variant, dismissible = true, autoDismissAfterMs = 0, title, detail, ondismissed }: Props = $props();
 
   onMount(() => {
     if (autoDismissAfterMs > 0) {
@@ -23,12 +25,17 @@
   });
 </script>
 
+<!-- teal-50: #f0fdfa -->
 <div
-  class="relative flex items-center overflow-hidden rounded-xl border border-rose-400 bg-rose-100 p-4"
+  class={`relative flex items-center overflow-hidden rounded-xl border  p-4 ${variant === 'error' ? 'border-rose-400 bg-rose-100' : 'border-primary bg-[#f0fdfa]'}`}
   in:fly={{ x: 200 }}
   out:fade={{ duration: 200 }}
 >
-  <WarningRegularIcon class="h-6 w-6 min-w-6 text-rose-400" />
+  {#if variant === 'error'}
+    <WarningRegularIcon class="h-6 w-6 min-w-6 text-rose-400" />
+  {:else}
+    <InfoRegularIcon class="size-6 min-w-6 text-primary" />
+  {/if}
   <div class="ml-4 mr-2 flex w-full flex-col overflow-hidden text-ellipsis break-words">
     <p class="truncate text-[12px]/[20px] font-bold text-slate-800">
       {title}
@@ -37,7 +44,9 @@
       <p class="line-clamp-2 text-[12px]/[16px] text-slate-800">{detail}</p>
     {/if}
   </div>
-  <button class="-mr-2 rounded-full p-3" onclick={() => ondismissed?.()}>
-    <XRegularIcon class="h-4 w-4 text-slate-800" />
-  </button>
+  {#if dismissible}
+    <button class="-mr-2 rounded-full p-3" onclick={() => ondismissed?.()}>
+      <XRegularIcon class="h-4 w-4 text-slate-800" />
+    </button>
+  {/if}
 </div>
