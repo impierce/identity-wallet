4dda54bb50983194ed1923e5ebfac1db9955e64e
Nander Stabel <nander.stabel@impierce.com>
2025-08-13 22:21:34 +0200

    fix: remove unused dependencies

--- unime/src-tauri/gen-static/android/app/build.gradle.kts
+++ unime/src-tauri/gen-static/android/app/build.gradle.kts
@@ -117,7 +117,7 @@ dependencies {
     implementation("androidx.webkit:webkit:1.6.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
-    implementation("rustls:rustls-platform-verifier:latest.release")
+    testImplementation("junit:junit:4.13.2")
     implementation(libs.rustls.platform.verifier)
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
