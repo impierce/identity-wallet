89e2ea28912fc7d497b880b9c4b6bbe0863cfac9
Daniel Mader <daniel.mader@impierce.com>
2025-09-09 13:42:46 +0200

    refactor: rename field

--- identity-wallet/src/state/credentials/actions/refresh_credential_status.rs
+++ identity-wallet/src/state/credentials/actions/refresh_credential_status.rs
@@ -8,7 +8,7 @@ use ts_rs::TS;
 #[derive(Serialize, Deserialize, Debug, TS, Clone)]
 #[ts(export, export_to = "bindings/actions/RefreshCredentialStatus.ts")]
 pub struct RefreshCredentialStatus {
-    pub credential_id: Option<String>,
+    pub id: Option<String>,
 }
 
 #[typetag::serde(name = "[Credential] Refresh Status")]
