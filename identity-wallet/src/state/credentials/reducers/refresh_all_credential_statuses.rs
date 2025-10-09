c5161a5b22625f8d50cfb6d69007de3e0da5b345
Oran Dan <oran-dan@hotmail.com>
2025-10-09 11:55:06 +0200

    chore: fix current_user_prompt

--- identity-wallet/src/state/credentials/reducers/refresh_all_credential_statuses.rs
+++ identity-wallet/src/state/credentials/reducers/refresh_all_credential_statuses.rs
@@ -4,18 +4,20 @@ use std::sync::Arc;
 use crate::{
     error::AppError,
     state::{
-        actions::Action,
+        actions::{listen, Action},
+        common::actions::unlock_storage::UnlockStorage,
         credentials::{
             actions::refresh_credential_status::RefreshCredentialStatus,
             reducers::refresh_credential_status::refresh_credential_status,
         },
+        user_prompt::CurrentUserPrompt,
         AppState,
     },
 };
 
 // TODO: test for possible poor latency/performance due to many stronghold interactions when many credentials need to be refreshed.
 /// Refreshes the credential status for all credentials in the state.
-pub async fn refresh_all_credential_statuses(state: AppState, _action: Action) -> Result<AppState, AppError> {
+pub async fn refresh_all_credential_statuses(state: AppState, action: Action) -> Result<AppState, AppError> {
     let credential_ids: Vec<String> = state
         .credentials
         .iter()
@@ -30,8 +32,15 @@ pub async fn refresh_all_credential_statuses(state: AppState, _action: Action) -
         state = refresh_credential_status(state, Arc::new(RefreshCredentialStatus { credential_id })).await?;
     }
 
+    // This line is needed to pass the correct `current_user_prompt` to the new state.
+    // The `current_user_prompt` differs depending on where this reducer will be used (currently only in UnlockStorage).
+    // So UnlockStorage needs the redirect prompt, most likely future use of this reducer in other locations won't.
+    let redirect_prompt = listen::<UnlockStorage>(action).map(|_unlock_storage| CurrentUserPrompt::Redirect {
+        target: "me".to_string(),
+    });
+
     Ok(AppState {
-        current_user_prompt: None,
+        current_user_prompt: redirect_prompt,
         ..state
     })
 }
