10d303c49e851f8fb34fa0cdfd785ae6a9976c85
Soula Andrikopoulos <soula.andrikopoulos@impierce.com>
2025-10-14 15:03:50 +0200

    feat: add 'terms of use' modal to 'about us' page with scrollable content

--- unime/src/routes/(app)/me/settings/about/+page.svelte
+++ unime/src/routes/(app)/me/settings/about/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import LL from '$i18n/i18n-svelte';
+  import { fade, scale } from 'svelte/transition';
+
+  import { createDialog, melt } from '@melt-ui/svelte';
 
   import { Toast, TopNavBar } from '$lib/components';
   import { dispatch } from '$lib/dispatcher';
-  import { HeartFillIcon } from '$lib/icons';
+  import { HeartFillIcon, XBoldIcon } from '$lib/icons';
   import UniMeLogo from '$lib/static/svg/logo/UniMeLogo.svelte';
   import { state as appState } from '$lib/stores';
 
@@ -37,6 +40,15 @@
       return false;
     }
   });
+
+  // Modal logic here
+  const {
+    elements: { trigger, overlay, content, description, close, portalled },
+    states: { open },
+  } = createDialog({
+    // Portal to root layout inside the `div` that sets the `dark` class. This ensures that dark mode works.
+    portal: '#portal',
+  });
 </script>
 
 <TopNavBar on:back={() => history.back()} title={$LL.SETTINGS.SUPPORT.ABOUT.NAVBAR_TITLE()} class="sticky top-0 z-10" />
@@ -65,7 +77,7 @@
         </dl>
       </section>
     {/if}
-    <section class="mb-4 flex flex-col items-center">
+    <section class=" flex flex-col items-center">
       <h2 class="font-bold">{$LL.SETTINGS.SUPPORT.ABOUT.VERSION()}</h2>
       <div class="mb-3">0.10.0</div>
       <div class="flex items-center">
@@ -73,13 +85,170 @@
         <HeartFillIcon class="pl-1" />
       </div>
     </section>
-    <section class="mb-4 flex flex-col items-center">
-      <h2 class="font-bold">{$LL.SETTINGS.SUPPORT.ABOUT.LICENSE()}</h2>
-      <div class="mb-3">Apache License 2.0</div>
-      <div>{`${new Date().getFullYear()} Impierce Technologies B.V.`}</div>
+
+    <!-- Terms and Conditions button -->
+    <section class=" flex flex-col items-center">
+      <button
+        data-component="Terms & Conditions"
+        use:melt={$trigger}
+        class="flex justify-between rounded-xl border border-slate-200 bg-white p-4 dark:border-slate-600 dark:bg-dark"
+      >
+        <div class="grow text-left">
+          <p class="text-[13px]/[24px] font-medium text-slate-800 dark:text-grey">
+            {$LL.ONBOARDING.TERMS.T_AND_C.TITLE()}
+          </p>
+          <p class="text-[12px]/[20px] font-medium text-slate-500 dark:text-slate-300">Read Terms and Conditions.</p>
+        </div>
+      </button>
     </section>
+    <!-- License -->
+    <div>
+      <section class="mb-4 flex flex-col items-center">
+        <h2 class="font-bold">{$LL.SETTINGS.SUPPORT.ABOUT.LICENSE()}</h2>
+        <div class="mb-3">Apache License 2.0</div>
+        <div>{`${new Date().getFullYear()} Impierce Technologies B.V.`}</div>
+      </section>
+    </div>
   </div>
 </div>
+
+<!-- The modal -->
+{#if $open}
+  <div use:melt={$portalled}>
+    <!-- Dimmed background -->
+    <div use:melt={$overlay} class="fixed inset-0 z-50 bg-black/50" transition:fade={{ duration: 150 }}></div>
+    <!-- Modal container -->
+    <div
+      data-component="Modal"
+      use:melt={$content}
+      class="fixed left-1/2 top-1/2 z-50 max-h-[85vh] w-[90vw]
+            -translate-x-1/2 -translate-y-1/2 overflow-y-auto rounded-xl
+            bg-background p-6 shadow-lg"
+      transition:scale={{ duration: 150, start: 0.96 }}
+    >
+      <!-- Modal content -->
+      <!-- <h2 use:melt={$title} class="mb-4 text-lg font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.DIALOG_TITLE()}
+      </h2>-->
+      <h2 class="mb-4 text-lg font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.DIALOG_TITLE()}
+      </h2>
+
+      <p class="mb-2 text-xs font-medium text-slate-500 dark:text-slate-300">
+        {$LL.ONBOARDING.TERMS.T_AND_C.LAST_UPDATED()}
+      </p>
+      <p use:melt={$description} class="rounded-lg text-xs font-medium">
+        {$LL.ONBOARDING.TERMS.T_AND_C.TL_DR()}
+      </p>
+      <!-- acts as <hr> -->
+      <div class="my-2 h-px w-full bg-brand"></div>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.AGREEMENT.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.AGREEMENT.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.DEFINITIONS.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.DEFINITIONS.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.USER_RESPONSIBILITIES.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.USER_RESPONSIBILITIES.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.DATA_OWNERSHIP.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.DATA_OWNERSHIP.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.DATA_VISIBILITY.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.DATA_VISIBILITY.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.INTELLECTUAL_PROPERTY_RIGHTS.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.INTELLECTUAL_PROPERTY_RIGHTS.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.PROHIBITED_ACTIVITIES.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.PROHIBITED_ACTIVITIES.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.THIRD_PARTY_SERVICES.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.THIRD_PARTY_SERVICES.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.LIABILITY.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.LIABILITY.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.INDEMNIFICATION.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.INDEMNIFICATION.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.MODIFICATIONS.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.MODIFICATIONS.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.LAW_AND_JURISDIFICATION.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.LAW_AND_JURISDIFICATION.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.SEVERABILITY.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.SEVERABILITY.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.LANGUAGE.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.LANGUAGE.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.ENTIRE_AGREEMENT.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.ENTIRE_AGREEMENT.DESCRIPTION()}
+      </p>
+      <h3 class="my-1 text-xs font-bold">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.CONTACT.TITLE()}
+      </h3>
+      <p class="mb-3 text-xs font-light">
+        {$LL.ONBOARDING.TERMS.T_AND_C.FULL.CONTACT.DESCRIPTION()}
+      </p>
+      <!-- Close button -->
+      <button
+        use:melt={$close}
+        aria-label="close"
+        class="absolute right-2 top-2 flex size-7 items-center justify-center rounded-full border border-slate-200 bg-white p-1 dark:border-slate-600 dark:bg-dark"
+      >
+        <XBoldIcon class="size-5" />
+      </button>
+    </div>
+  </div>
+{/if}
 
 {#if showMessage()}
   <div class="absolute bottom-[calc(64px_+_16px_+_var(--safe-area-inset-bottom))] left-4 w-[calc(100%_-_32px)]">
