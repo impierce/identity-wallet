45815845353d387ce6fad940baa6a183c44588a5
Nander Stabel <nander.stabel@impierce.com>
2026-02-26 09:38:13 +0100

    refactor: use OnceLock for `Resolver` in Subject`

--- identity-wallet/src/state/common/reducers/unlock_storage.rs
+++ identity-wallet/src/state/common/reducers/unlock_storage.rs
@@ -17,28 +17,27 @@ pub async fn unlock_storage(state: AppState, action: Action) -> Result<AppState,
         let mut state_guard = state.core_utils.managers.lock().await;
 
         let stronghold_manager = Arc::new(StrongholdManager::load(&password).map_err(StrongholdLoadingError)?);
-        // let resolver = state.core_utils.resolver().await;
 
-        // let subject = subject(stronghold_manager.clone(), password, resolver).await;
+        let subject = subject(stronghold_manager.clone(), password).await;
 
-        // let provider_manager = ProviderManager::new(
-        //     subject.clone(),
-        //     Vec::from(SUPPORTED_DID_METHODS),
-        //     Vec::from(SUPPORTED_SIGNING_ALGORITHMS),
-        // )
-        // .map_err(OID4VCProviderManagerError)?;
-        // let wallet: Wallet = Wallet::new(
-        //     subject.clone(),
-        //     Vec::from(SUPPORTED_DID_METHODS),
-        //     Vec::from(SUPPORTED_SIGNING_ALGORITHMS),
-        // )
-        // .map_err(OID4VCWalletError)?;
+        let provider_manager = ProviderManager::new(
+            subject.clone(),
+            Vec::from(SUPPORTED_DID_METHODS),
+            Vec::from(SUPPORTED_SIGNING_ALGORITHMS),
+        )
+        .map_err(OID4VCProviderManagerError)?;
+        let wallet: Wallet = Wallet::new(
+            subject.clone(),
+            Vec::from(SUPPORTED_DID_METHODS),
+            Vec::from(SUPPORTED_SIGNING_ALGORITHMS),
+        )
+        .map_err(OID4VCWalletError)?;
 
-        // state_guard.identity_manager.replace(IdentityManager {
-        //     subject,
-        //     provider_manager,
-        //     wallet,
-        // });
+        state_guard.identity_manager.replace(IdentityManager {
+            subject,
+            provider_manager,
+            wallet,
+        });
 
         info!("loading credentials from stronghold");
 
