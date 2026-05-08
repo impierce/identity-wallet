54f1d527993be927052fff83f6b2837ab9302d21
Nander Stabel <nander.stabel@impierce.com>
2026-05-08 14:23:20 +0200

    refactor: remove unused patches for identity dependencies in Cargo files

--- identity-wallet/src/state/credentials/reducers/handle_oid4vp_authorization_request.rs
+++ identity-wallet/src/state/credentials/reducers/handle_oid4vp_authorization_request.rs
@@ -21,7 +21,7 @@ use identity_core::common::Object as IotaObject;
 use identity_credential::sd_jwt_vc::SdJwtVc;
 use identity_iota::credential::{EnvelopedVc, VcDataUrl};
 use identity_iota::did::CoreDID;
-use log::{info, warn};
+use log::info;
 use oid4vc::oid4vc_core::types::string_or_object::StringOrObject;
 use oid4vc::oid4vc_core::{
     authorization_request::{AuthorizationRequest, Object},
