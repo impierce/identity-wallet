e22d65f919eb89c4c3709517e142fbe423e33b82
Coplat <chaya777@proton.me>
2026-08-10 18:57:26 +0200

    init: trust_verificaiton struct

---
+++ unime/src/lib/components/TrustShield.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  // remove before commit!!
+  const trust_verification = {
+    status: 'Verified',
+    ecosystem_name: 'Findynet Network',
+    ecosystem_id: 'https://trust-anchor.findynet.fi',
+    ecosystem_logo_uri: null,
+    previously_trusted: true,
+  } as const;
+</script>
+
