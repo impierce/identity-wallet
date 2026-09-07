b4b8439afcaf63e03fcd0305b8622702578a7ec4
Coplat <chaya777@proton.me>
2026-09-07 10:47:00 +0200

    nit: remove unnecessary comments

--- unime/src/lib/utils.test.ts
+++ unime/src/lib/utils.test.ts
@@ -117,7 +117,6 @@ describe('formatRelativeDateTime function', () => {
     expect(formatRelativeDateTime(twoDaysAgo.toISOString(), 'de-DE')).toEqual('Vorgestern');
   });
 
-  // This fixture has to be a result that starts with a letter, not a digit.
   test('1 day ago en-GB, uncapitalized', () => {
     const now = new Date();
     const oneDayAgo = new Date(now.setDate(now.getDate() - 1));
